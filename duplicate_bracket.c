a4fe59d41c9adeddf7eeed72e4f1aeea92dd32cc
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-09-16 22:44:12 +0530

    duplicate bracket

--- duplicate_bracket.c
+++ duplicate_bracket.c
@@ -1,14 +1,14 @@
 //Owner: Kumar Ankit
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 20
+#define MAX 50
 
 typedef struct{
-    int data[MAX];
+    char data[MAX];
     int top;
 }STACK;
 
-int push(STACK *S, int v){
+int push(STACK *S, char v){
     if(S->top == MAX-1){
         printf("Overflow\n");
         return 1;
@@ -18,7 +18,7 @@ int push(STACK *S, int v){
     return 0;
 }
 
-int pop(STACK *S, int *v){
+int pop(STACK *S, char *v){
     if(S->top == -1){
         printf("Underflow\n");
         return 1;
@@ -30,19 +30,38 @@ int pop(STACK *S, int *v){
 
 void display(STACK *S){
     if(S->top == -1) return;
-    int u;
+    char u;
     pop(S, &u);
-    printf("%d ", u);
+    printf("%c ", u);
     display(S);
     push(S, u);
 }
 
+void duplicate_bracket_finder(char *a){
+    STACK temp;
+    temp.top = -1;
+    int i = 0;
+    while(a[i]!='\0'){
+        if(a[i]==')'){
+            char m;
+            pop(&temp, &m);
+            if(m=='('){
+                printf("Duplicate bracket found.\n");
+                return;
+            }
+            while(m!='('){
+                pop(&temp, &m);
+            }
+        }else{
+            push(&temp, a[i]);
+        }
+        i++;
+    }
+    printf("No duplicate bracket found.\n");
+}
+
 int main(){
-    int m;
-    STACK S1;
-    S1.top = -1;
-    int k = push(&S1, 15);
-    display(&S1);
-    int q = pop(&S1, &m);
+    char str[] = "A+((B*(d-k)))+R";
+    duplicate_bracket_finder(str);
     return 0;
 }
