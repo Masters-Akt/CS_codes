c379227adc5dfaad48a8ff7c693aab959998d8d5
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-10-17 23:16:14 +0530

    tree

--- tree.c
+++ tree.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node{
+struct node{ //tree structure
     struct node *left;
     int data;
     struct node *right;
 };
 
-struct node * create_node(int v){
+struct node * create_node(int v){ //node creation
     struct node *curr = (struct node *)malloc(sizeof(struct node));
     if(curr!=NULL){ //if curr == NULL, memory shortage
         curr->left = curr->right = NULL;
@@ -17,21 +17,21 @@ struct node * create_node(int v){
     return curr;
 }
 
-void inorder(struct node *r){
+void inorder(struct node *r){ //inorder search using recursion
     if(r==NULL) return;
     inorder(r->left);
     printf("%d ", r->data);
     inorder(r->right);
 }
 
-void preorder(struct node *r){
+void preorder(struct node *r){ //preorder search using recursion
     if(r==NULL) return;
     printf("%d ", r->data);
     preorder(r->left);
     preorder(r->right);
 }
 
-void postorder(struct node *r){
+void postorder(struct node *r){ //postorder search using recursion
     if(r==NULL) return;
     postorder(r->left);
     postorder(r->right);
@@ -39,6 +39,12 @@ void postorder(struct node *r){
 }
 
 int main(){
-
+    struct node *root = NULL;
+    root = create_node(4);
+    root->left = create_node(9);
+    root->left->left = create_node(3);
+    root->right = create_node(8);
+    root->right->left = create_node(7);
+    root->right->right = create_node(6);
     return 0;
 }
