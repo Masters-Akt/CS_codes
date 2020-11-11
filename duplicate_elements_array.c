44efd6672ebe9a0a8699fd00242e57b5a7cd08cd
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-11 08:30:39 +0530

    duplicate array elements

---
+++ duplicate_elements_array.c
@@ -0,0 +1,25 @@
+//Kumar Ankit
+#include<stdio.h>
+
+void find_repeated(int a[], int n){
+    int count = 0;
+    printf("Repeating elements are: ");
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                printf("%d ", a[j]);
+            }
+        }
+    }
+}
+
+int main(){
+    printf("Enter size of the array : ");
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter elements : ");
+    for(int i=0;i<n;i++) scanf("%d", &a[i]);
+    find_repeated(a, n);
+    return 0;
+}
