e948fc7a28ed70318faa3383d8962588ea177a87
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-12 20:31:09 +0530

    bubble sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -3,7 +3,18 @@
 
 void bubble_sort(int a[], int n){
     int i, j, temp;
-    
+    for(i=0;i<n-1;i++){
+        int count = 0;
+        for(j=0;j<n-1-i;j++){
+            if(a[j]>a[j+1]){
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                count = 1;
+            }
+        }
+        if(count==0) break;
+    }
 }
 
 int main(){
