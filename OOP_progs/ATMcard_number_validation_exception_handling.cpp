02a08b5085604e59d33ea388123637c6230fd653
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-13 13:28:43 +0530

    exception handling in c++

--- OOP_progs/ATMcard_number_validation_exception_handling.cpp
+++ OOP_progs/ATMcard_number_validation_exception_handling.cpp
@@ -1,3 +1,16 @@
+//Kumar Ankit
+/*Question : 
+A bank generated credit cards
+---card number must be 16 digits
+---starting 2 digits must be either 50 or 51
+---last four digits can't be zero
+---the checksum of alternative digits are computed and the difference should be zero
+For example, if the card number is 5011213678920911
+Then, 5+1+2+3+7+9+0+1=28
+0+1+1+6+8+2+9+1=28
+So, its a valid card and accept it and otherwise generate proper exception.
+
+*/
 #include<iostream>
 #include<fstream>
 #include<string.h>
@@ -17,7 +30,7 @@ bool checksum(string s){
 }
 
 void check_card(string s){
-    if(s.length()!=16 || s[0]!='5' || !(s[1]=='0' || s[1]=='1') || checksum(s)==false){
+    if(s.length()!=16 || s[0]!='5' || !(s[1]=='0' || s[1]=='1') || checksum(s)==false || (s[15]=='0' && s[14]=='0' && s[13]=='0' && s[12]=='0')){
         throw "Not Valid";
     }else
     {
