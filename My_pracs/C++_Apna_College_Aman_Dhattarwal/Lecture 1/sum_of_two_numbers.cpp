4bdf3287fe71f0c9bf1d865e78cdbc80b11a04b4
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-16 23:13:08 +0530

    Sum of two numbers

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 1/sum_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter two numbers : ";
+    int a, b;
+    cin>>a>>b;
+    cout<<"The sum is = "<<a+b<<endl;
+    return 0;
+}
