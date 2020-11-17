6d5c4874066671fc53b0a9e0663ff2aca4e08f7f
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 17:38:53 +0530

    odd even

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 1/odd_even_no.cpp
@@ -0,0 +1,12 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter a number : ";
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<"Even!"<<endl;
+    else cout<<"Odd!"<<endl;
+    return 0;
+}
