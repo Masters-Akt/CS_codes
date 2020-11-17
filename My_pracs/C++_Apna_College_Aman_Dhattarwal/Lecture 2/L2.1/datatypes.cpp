3af592d2de3ed284bd54ae31ccbe0e9a60c83118
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 18:03:58 +0530

    data types and their size

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 2/L2.1/datatypes.cpp
@@ -0,0 +1,21 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Size of int = "<<sizeof(a)<<endl;
+    float b;
+    cout<<"Size of float = "<<sizeof(b)<<endl;
+    char x;
+    cout<<"Size of char = "<<sizeof(x)<<endl;
+    double d;
+    cout<<"Size of double = "<<sizeof(d)<<endl;
+    bool c;
+    cout<<"Size of bool = "<<sizeof(c)<<endl;
+    short int si;
+    long int li;
+    cout<<"Size of long int = "<<sizeof(li)<<endl;
+    cout<<"Size of short int = "<<sizeof(si)<<endl;
+    return 0;
+}
