289abc763bab7099f1ff1de8ec1480178f5b4739
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 17:36:26 +0530

    temp conv

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 1/temp_converter_F_to_C.cpp
@@ -0,0 +1,11 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"ENter temperature in F : ";
+    float f;
+    cin>>f;
+    cout<<"Temperature in C = "<<((f-32)*5)/9<<"C.";
+    return 0;
+}
