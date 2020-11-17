56d60ff3c4a428d0705794a20c554a31371c854e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 17:57:11 +0530

    avg of 2 nos

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 2/L2.1/avg_2_nos.cpp
@@ -0,0 +1,11 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter two numbers : ";
+    int a,b;
+    cin>>a>>b;
+    cout<<"Average = "<<(float)(a+b)/2<<endl;
+    return 0;
+}
