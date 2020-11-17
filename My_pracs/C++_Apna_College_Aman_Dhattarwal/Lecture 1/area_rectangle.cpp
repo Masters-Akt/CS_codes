56dad2fc4f9658e0856b98213da67524a65d9df7
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 17:27:48 +0530

    area of rectangle

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 1/area_rectangle.cpp
@@ -0,0 +1,11 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout<<"Enter lenght and breadth : ";
+    cin>>a>>b;
+    cout<<"Area of the rectangle is : "<<a*b<<endl;
+    return 0;
+}
