0c8758d8de0632f501f6e01755a8a667840a5e1d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 17:42:05 +0530

    Sum till n

---
+++ My_pracs/C++_Apna_College_Aman_Dhattarwal/Lecture 1/sum_n_natural_nos.cpp
@@ -0,0 +1,11 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter n : ";
+    int n;
+    cin>>n;
+    cout<<"Sum till n = "<<(n*(n+1))/2<<endl;
+    return 0;
+}
