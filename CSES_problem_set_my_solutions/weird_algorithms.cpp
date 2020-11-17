fd688409c902f3b196b929af652f9383b0b4b9cf
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-11-17 23:47:07 +0530

    weird algorithms

---
+++ CSES_problem_set_my_solutions/weird_algorithms.cpp
@@ -0,0 +1,16 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin>>n;
+    while(true){
+        cout<<n<<" ";
+        if(n==1) break;
+        if(n%2==0) n/=2;
+        else n=n*3+1;
+    }
+    cout<<"\n";
+    return 0;
+}
