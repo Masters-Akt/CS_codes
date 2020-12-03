14ffef28c43dbbb854535b9dcde521686a41e73b
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-03 18:59:06 +0530

    voting eligibility

---
+++ C++_Apna_College_Aman_Dhattarwal/Lecture 2.3/vote_or_not.cpp
@@ -0,0 +1,11 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cin>>age;
+    if(age>=18) cout<<"You can vote!\n";
+    else cout<<"You cannot vote!\n";
+    return 0;
+}
