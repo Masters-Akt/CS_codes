f49e053f7b2d8ca71a562173308c5dd70bcaaaef
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-03 19:30:31 +0530

    do while print till positive entered

---
+++ C++_Apna_College_Aman_Dhattarwal/Lecture 2.4/print_till_entered_positive_number_dowhile.cpp
@@ -0,0 +1,13 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    do{
+        cout<<n<<endl;
+        cin>>n;
+    }while(n>0);
+    return 0;
+}
