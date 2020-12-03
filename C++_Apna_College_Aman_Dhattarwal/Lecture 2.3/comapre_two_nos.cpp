cef68dd6bb0f1c2f9f1518efb2c40c98af7cdcef
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-03 19:02:02 +0530

    compare nos

---
+++ C++_Apna_College_Aman_Dhattarwal/Lecture 2.3/comapre_two_nos.cpp
@@ -0,0 +1,12 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x==y) cout<<"Both are equal\n";
+    else if(x>y) cout<<"X is greater than Y\n";
+    else cout<<"X is less than Y\n";
+    return 0;
+}
