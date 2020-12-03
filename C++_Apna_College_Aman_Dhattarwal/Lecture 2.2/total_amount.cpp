3895ac3cda2cccd8845d2a45d004296ba005683d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-03 18:39:44 +0530

    total amount

---
+++ C++_Apna_College_Aman_Dhattarwal/Lecture 2.2/total_amount.cpp
@@ -0,0 +1,12 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int amount1;
+    cin>>amount1;
+    int amount2;
+    cin>>amount2;
+    cout<<"Total amount = "<<amount1+amount2<<endl;
+    return 0;
+}
