06f6a60c5ef1729dc413f6864ddf249e6cf490a1
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-08 11:24:09 +0530

    basics

---
+++ Javascript/basics/variables.js
@@ -0,0 +1,12 @@
+var fullName = "Kumar Ankit";
+console.log(fullName);
+var courseName = "Starting to learn Js";
+console.log(courseName);
+var isLoggedIn = true;
+console.log(isLoggedIn);
+var loggedCount = 56;
+console.log(loggedCount);
+var paymentMode;
+console.log(paymentMode); //gives undefined
+paymentMode = "Memory Card";
+console.log(paymentMode);
