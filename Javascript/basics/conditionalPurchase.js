a782ffdd4dfa6622445ec37ebbf5ffe480bfdb61
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-09 10:24:28 +0530

    conditionals

---
+++ Javascript/basics/conditionalPurchase.js
@@ -0,0 +1,8 @@
+var isLoggedIn = true;
+var isEmailVerified = false;
+var cardInfo = true;
+if(isLoggedIn && isEmailVerified && cardInfo){
+    console.log("Allow User to make a Purchase!");
+}else{
+    console.log("You are not alllowed to make any purchase!");
+}
---
+++ Javascript/basics/conditionals.js
@@ -0,0 +1,11 @@
+var temperature;
+//TODO: Go to google and get the data
+temperature = 39;
+if(temperature < 20){
+    console.log("Its cold outside!");
+}
+if(temperature < 30){
+    console.log("Its moderate outside!");
+}else{
+    console.log("Its really hot outside!");
+}
