12b4e43ed87a9dd4286787cef0d949078fca210a
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-09 10:39:24 +0530

    conditionals

---
+++ Javascript/basics/conditionalSignout.js
@@ -0,0 +1,8 @@
+//Show sign in button if not signed in, else show sign out button
+var authenticated = true; //logged in
+// if(authenticated){
+//     console.log("Show sign out button!");
+// }else{
+//     console.log("Show sign in button!");
+// }
+authenticated ? console.log("Show sign out button!") : console.log("Show sign in button!");
