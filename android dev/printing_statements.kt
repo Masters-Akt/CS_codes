fb3e2cfa06ed615be5acc29445ecf3d9907378a8
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-13 11:28:03 +0530

    bday cake

--- android dev/printing_statements.kt
+++ android dev/printing_statements.kt
@@ -2,4 +2,12 @@ fun main(){
     println("Happy Birthday!");
     println("Kumar Ankit!");
     println("You are 21!");
+    printfln("");
+    println("   ,,,,,   ");
+    println("   |||||   ");
+    println(" =========");
+    println("@@@@@@@@@@@");
+    println("{@~@~@~@~@}");
+    println("@@@@@@@@@@@");
+    println("");
 }
