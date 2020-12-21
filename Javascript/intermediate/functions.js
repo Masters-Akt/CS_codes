617612a142a491a94d0a2b38f5a073e8ffe3b975
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-21 18:40:23 +0530

    basics of function

---
+++ Javascript/intermediate/functions.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log("Hello there, Ankit!");
+    console.log(`Hello there, ${name}. How are you?`);
+}
+
+sayHello();
+sayHello("Kumar");
+
+function namastey(){
+    return "Hello in India!";
+}
+
+var greetings = namastey();
+console.log(greetings);
+console.log(namastey());
