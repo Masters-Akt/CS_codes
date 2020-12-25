72ec098fd1f84c610574aa5568a74de618250fa5
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-25 20:24:40 +0530

    jumps

---
+++ C++_Apna_College_Aman_Dhattarwal/Lecture 3.1/jumps.cpp
@@ -0,0 +1,16 @@
+//Kumar Ankit
+#include<iostream>
+using namespace std;
+
+int main(){
+    int pocketMoney = 3000;
+    for(int date=1;date<=30;date++){
+        if(date%2==0){
+            continue;
+        }
+        if(pocketMoney==0) break;
+        cout<<"Go out today!"<<endl;
+        pocketMoney-=300;
+    }
+    return 0;
+}
