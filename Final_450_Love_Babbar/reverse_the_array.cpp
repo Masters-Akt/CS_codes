cd520b7f968a12112ae446c267119cbb7d9ed02e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-28 00:41:59 +0530

    reverse array iterative approach

--- Final_450_Love_Babbar/reverse_the_array.cpp
+++ Final_450_Love_Babbar/reverse_the_array.cpp
@@ -2,6 +2,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void iterative_method(int a[], int start, int end){
+    int n = end+1;
+    while(start<end){
+        int temp = a[start];
+        a[start] = a[end];
+        a[end] = temp;
+        start++;
+        end--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
 string first_try(string s){
     string ans="";
     for(int i=s.length()-1;i>=0;i--){
@@ -12,7 +27,10 @@ string first_try(string s){
 
 int main(){
     string s;
-    cin>>s;
-    cout<<first_try(s);
+    s = "sadggvcvjkabj";
+    cout<<first_try(s)<<endl;
+    int a[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = 10;
+    iterative_method(a, 0, n-1);
     return 0;
 }
