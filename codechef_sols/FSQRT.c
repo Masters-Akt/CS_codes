c5e5008c8cdc79ce06edf5f7d302ec8c2788c9d1
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2020-12-28 13:30:06 +0530

    FSQRT

---
+++ codechef_sols/FSQRT.c
@@ -0,0 +1,16 @@
+//Kumar Ankit
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+	int t,n,r;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&n);
+	    r=sqrt(n);
+	    printf("%d\n",r);
+	}
+	return 0;
+}
+
