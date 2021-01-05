259f9462678487eea70e151d7f67aefe76c99da0
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-05 22:15:36 +0530

    FLOW008

---
+++ codechef_sols/FLOW008.c
@@ -0,0 +1,18 @@
+//Kumar Ankit
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,n;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&n);
+	    if(n<10)
+	    printf("What an obedient servant you are!\n");
+	    else
+	    printf("-1\n");
+	}
+	return 0;
+}
+
