601b93127c18c0efccd8836a0cea60180e86a0b3
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-06 18:54:38 +0530

    DIFFSUM

---
+++ codechef_sols/DIFFSUM.c
@@ -0,0 +1,15 @@
+//Kumar Ankit
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int m,n,d,s;
+	scanf("%d",&m);
+	scanf("%d",&n);
+	if(m>n)
+	printf("%d\n",(m-n));
+	else
+	printf("%d\n",(m+n));
+	return 0;
+}
+
