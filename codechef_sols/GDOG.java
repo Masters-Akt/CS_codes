5e35096803e00240a14310147a7525a0e315f866
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-08 19:57:27 +0530

    GDOG

---
+++ codechef_sols/GDOG.java
@@ -0,0 +1,27 @@
+//Kumar Ankit
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int k=sc.nextInt();
+		    int max=0;
+		    for(int i=1;i<=k;i++){
+		        if(n%i>max)
+		        max=n%i;
+		    }
+		    System.out.println(max);
+		}
+	}
+}
