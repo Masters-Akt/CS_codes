1bdf8c2391a7a92714bf05e3504521baa0e90162
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-08 19:33:40 +0530

    CLOCK01

---
+++ codechef_sols/CLOCK01.java
@@ -0,0 +1,20 @@
+//Kumar Ankit
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int h=sc.nextInt();
+		int m=sc.nextInt();
+		double a=Math.abs((5.5*m)-(30*h));
+		System.out.println(a);
+	}
+}
