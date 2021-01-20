a28a940d629eb1145c7e96663d8bc3dafbb6a668
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-20 13:44:49 +0530

    prime nos 1 to 100

--- shell scripting/display_prime_1_to_100.sh
+++ shell scripting/display_prime_1_to_100.sh
@@ -1,2 +1,16 @@
-echo "Displaying prime numbers between 1 and 100 : "
-
+echo Displaying prime numbers between 1 and 100 : 
+for i in `seq 2 1 100`
+do
+	c=0
+	for j in `seq 2 1 $i`
+	do
+		if [ `expr $i % $j` == 0 ]
+		then
+			c=`expr $c + 1`
+		fi
+	done
+	if [ $c == 1 ]
+	then
+		echo $i
+	fi
+done
