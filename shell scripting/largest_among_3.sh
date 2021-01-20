bf2c59fa66657dd56ce2d0e4c885e5a0cd025e4f
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-20 11:59:23 +0530

    greatest among 3

--- shell scripting/largest_among_3.sh
+++ shell scripting/largest_among_3.sh
@@ -1,22 +1,22 @@
-echo "Enter first number : "
+echo Enter first number :
 read A
-echo "Enter second number : "
+echo Enter second number :
 read B
-echo "Enter third number : "
+echo Enter third number :
 read C
 if [ $A -gt $B ]
 then
 	if [ $A -gt $C ]
 	then
-		echo "$A is greatest."
+		echo $A is greatest.
 	else
-		echo "$C is greatest."
+		echo $C is greatest.
 	fi
 else
 	if [ $B -gt $C ]
 	then
-		echo "$B is greatest."
+		echo $B is greatest.
 	else
-		echo "$C is greatest."
+		echo $C is greatest.
 	fi
 fi
