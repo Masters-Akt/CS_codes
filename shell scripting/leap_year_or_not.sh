967cd46d02b2651276771945991e36d41d3976ab
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-20 11:43:28 +0530

    leap year or not

--- shell scripting/leap_year_or_not.sh
+++ shell scripting/leap_year_or_not.sh
@@ -1,13 +1,13 @@
-echo "Enter year YYYY : "
+echo Enter year YYYY : 
 read year
 
 byfour=`expr $year % 4`
 byhundered=`expr $year % 100`
 byfourhundered=`expr $year % 400`
 
-if [ $byfour -eq 0 -a $byhundered -ne 0 -o $byfourhundered -eq 0 ];
+if [ $byfour -eq 0 -a $byhundered -ne 0 -o $byfourhundered -eq 0 ]
 then
-	echo "$year is a leap year."
+	echo $year is a leap year.
 else
-	echo "$year is not a leap year."
+	echo $year is not a leap year.
 fi
