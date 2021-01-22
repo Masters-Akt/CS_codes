1291bef2706243354184db4266865f8cfb1dd108
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-01-22 23:34:28 +0530

    maxmin

--- Final_450_Love_Babbar/Find_the_maximum_and_minimum_element_in_an_array.cpp
+++ Final_450_Love_Babbar/Find_the_maximum_and_minimum_element_in_an_array.cpp
@@ -2,6 +2,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Pair{
+    int max;
+    int min;
+};
+
+struct Pair minmaxf(int arr[], int n){
+    struct Pair minmax;
+    if(n==1){
+        minmax.max=arr[0];
+        minmax.min=arr[0];
+        return minmax;
+    }
+    if(arr[0]>arr[1]){
+        minmax.min=arr[1];
+        minmax.max=arr[0];
+    }else{
+        minmax.min=arr[0];
+        minmax.max=arr[1];
+    }
+    for(int i=2;i<n;i++){
+        if(arr[i]>minmax.max) minmax.max=arr[i];
+        if(arr[i]<minmax.min) minmax.min=arr[i];
+    }
+    return minmax;
+}
+
 int first_try(int a, int b, int c){
     if(a>b){
         if(b>c) return c;
@@ -18,5 +44,8 @@ int main(){
     int a=1, b=2, c=3;
     cout<<"Getting middle element:\n";
     cout<<first_try(a, b, c);
+    int arr[10]={10, 56, 34, 83, 90, 45, 18, 96, 49, 28};
+    struct Pair minmax = minmaxf(arr, 10);
+    cout<<"Maximum = "<<minmax.max<<" Minimum = "<<minmax.min<<endl;
     return 0;
 }
