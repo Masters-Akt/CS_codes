54e4a578dc648d473e8db236eff6113f384839c9
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 21:55:10 +0530

    CUTBOARD

---
+++ codechef_sols/CUTBOARD.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    System.out.println((n-1)*(m-1));
+		}
+	}
+}
