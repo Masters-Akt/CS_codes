f868d9ccda9d796477dc7cc705ec932544936a37
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 22:38:28 +0530

    UWCOI21A

---
+++ codechef_sols/UWCOI21A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    cout<<1<<" "<<n<<"\n";
+	}
+	return 0;
+}
