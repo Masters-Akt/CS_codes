93427703c0b06bde9e1093267f18c5b55045d25c
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 21:30:12 +0530

    SMPAIR

---
+++ codechef_sols/SMPAIR.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int a[]=new int[n];
+		    for(int i=0;i<n;i++){
+		        a[i]=sc.nextInt();
+		        
+		    }
+		    
+		    Arrays.sort(a);
+		    int min=a[0]+a[1];
+		    System.out.println(min);
+		}
+	}
+}
