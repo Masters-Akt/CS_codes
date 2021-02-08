7b8168b524fefb1c4be4f4531d5007cac537900f
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 22:16:22 +0530

    GVAWAY

---
+++ codechef_sols/GVAWAY.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long int t;
+	cin>>t;
+	while(t--){
+	    int l,r,k;
+	    cin>>l>>r>>k;
+	    if(r==l){
+	        cout<<1<<endl;
+	    }
+	    else{
+	        cout<<k<<endl;
+	    }
+	   
+	}
+	return 0;
+}
