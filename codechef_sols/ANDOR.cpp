c4056f95de59ed49facd101283084689d9a6a29f
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 22:36:23 +0530

    ANDOR

---
+++ codechef_sols/ANDOR.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long int n;
+	    cin>>n;
+	    cout<<n<<" "<<"0"<<endl;
+	}
+	return 0;
+}
