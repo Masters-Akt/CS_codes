f964116e68ef0e0fb0f51e0f70e6fe71e2b1b6af
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 22:31:22 +0530

    RTN011

---
+++ codechef_sols/RTN011.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    string s;
+	    cin>>s;
+	    stack<char> st;
+	    char x;
+	    bool ch=true;
+	    for(int i=0;i<s.length();i++){
+	        if(s[i]=='('||s[i]=='{'||s[i]=='['){
+	            st.push(s[i]);
+	            continue;
+	        }
+	        if(st.empty()){
+	            ch=false;
+	            break;
+	        }
+	        x=st.top();
+	        st.pop();
+	        if(s[i]==')'){
+	            if(x=='{'||x=='['){
+	                ch=false;
+	                break;
+	            }
+	        }
+	        else if(s[i]=='}'){
+	            if(x=='('||x=='['){
+	                ch=false;
+	                break;
+	            }
+	        }
+	        else{
+	            if(x=='('||x=='{'){
+	                ch=false;
+	                break;
+	            }
+	        }
+	    }
+	    if(ch==true){
+	    if(st.empty()){
+	        cout<<"YES\n";
+	    }
+	    else{
+	        cout<<"NO\n";
+	    }}
+	    else{
+	        cout<<"NO\n";
+	    }
+	    
+	}
+	return 0;
+}
