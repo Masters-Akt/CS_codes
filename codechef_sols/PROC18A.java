6e179e913025e97b2b6ffca1f69c51411048eff8
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-02-08 21:57:50 +0530

    PROC18A

---
+++ codechef_sols/PROC18A.java
@@ -0,0 +1,41 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int k=sc.nextInt();
+		    int a[]=new int[n];
+		    int sum=0;
+		    for(int i=0;i<n;i++){
+		        a[i]=sc.nextInt();
+		        sum+=a[i];
+		    }
+		    if(k>=n){
+		        System.out.println(sum);
+		    }
+		    else{
+		        int max=0;
+		        for(int i=0;i<n-k+1;i++){
+		            int s=0;
+		            for(int j=i;j<i+k;j++){
+		                s+=a[j];
+		            }
+		            if(s>max)
+		            max=s;
+		        }
+		        System.out.println(max);
+		    }
+		}
+	}
+}
