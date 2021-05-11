d72c959a7aca34a4545c4d612ba724ef7ba88974
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-11 20:56:40 +0530

    Number of Good Pairs

---
+++ leetcode_sol/Number_Of_Good_Pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
