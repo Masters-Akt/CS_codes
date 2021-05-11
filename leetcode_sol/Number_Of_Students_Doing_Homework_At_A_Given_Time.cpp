7fc94b1d76d19d57ac5b09c3e37c853dd696e092
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-11 22:37:03 +0530

    Number of students doing homework at a given time

---
+++ leetcode_sol/Number_Of_Students_Doing_Homework_At_A_Given_Time.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int count=0;
+        for(int i=0;i<startTime.size();i++){
+            
+                if(queryTime>=startTime[i] && queryTime<=endTime[i]){
+                    count++;
+                }
+            
+        }
+        return count;
+    }
+};
