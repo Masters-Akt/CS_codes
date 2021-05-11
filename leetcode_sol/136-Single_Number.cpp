cf0c36cb350ed576cadc244bd3649309e1df6244
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-11 23:03:38 +0530

    Single Number

---
+++ leetcode_sol/136-Single_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        sort(nums.begin(), nums.end());
+        for(int i=1;i<nums.size();i+=2){
+            if(nums[i]!=nums[i-1]) return nums[i-1];
+        }
+        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
+        return 0;
+    }
+};1
