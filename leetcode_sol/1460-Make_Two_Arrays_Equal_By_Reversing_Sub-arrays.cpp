b16687fa401c45da0257f9e2cde5d38d9d762196
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-11 22:53:54 +0530

    Make Two Arrays Equal By Reversing Sub-arrays

---
+++ leetcode_sol/1460-Make_Two_Arrays_Equal_By_Reversing_Sub-arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        if(target.size()!=arr.size()) return false;
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        for(int i=0;i<target.size();i++){
+            if(target[i]!=arr[i]) return false;
+        }
+        return true;
+    }
+};
