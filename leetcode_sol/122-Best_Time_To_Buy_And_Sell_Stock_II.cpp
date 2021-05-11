d7c7eb8415080de5bd7f3463352815f89580244e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-11 23:07:01 +0530

    Best Timr To Buy And Sell Stock II

---
+++ leetcode_sol/122-Best_Time_To_Buy_And_Sell_Stock_II.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sum=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>prices[i-1]) sum+=(prices[i]-prices[i-1]);
+        }
+        return sum;
+    }
+};
