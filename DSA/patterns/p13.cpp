b8f5d405435e212f75a9f17d4d91de44f02b845e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-16 23:32:47 +0530

    Star Pattern

--- DSA/patterns/p13.cpp
+++ DSA/patterns/p13.cpp
@@ -5,6 +5,7 @@ For n=4
     * * *
   * * * * *
 * * * * * * *
+* * * * * * *
   * * * * *
     * * *
       *
@@ -28,7 +29,7 @@ int main(){
         }
         cout<<endl;
     }
-    for(int i=n-1;i>=1;i--){
+    for(int i=n;i>=1;i--){
         int space = n-i;
         for(int j=1;j<=space;j++){
             cout<<"  ";
