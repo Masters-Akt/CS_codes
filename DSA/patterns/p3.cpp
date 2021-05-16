0df84cfbc3e36b8230c7ea871f2908edaad0393d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-16 21:47:22 +0530

    Half Inverted Pyramid

---
+++ DSA/patterns/p3.cpp
@@ -0,0 +1,22 @@
+//Inverted Half Pyramid
+/*
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
