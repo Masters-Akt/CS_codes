ad5c0fb181945e721da2ff2b64ea4a0b05209c59
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-16 22:47:28 +0530

    Method 2 for butterfly pattern

--- DSA/patterns/Butterfly_Pattern_p7.cpp
+++ DSA/patterns/Butterfly_Pattern_p7.cpp
@@ -32,3 +32,20 @@ int main(){
     }
     return 0;
 }
+
+/*
+Method 2
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<"* ";
+    }
+    int space = 2*n - 2*i;
+    for(int j=1;j<=space;j++){
+        cout<<"  ";
+    }
+    for(int j=1;j<=i;j++){
+        cout<<"* ";
+    }
+}
+*/
