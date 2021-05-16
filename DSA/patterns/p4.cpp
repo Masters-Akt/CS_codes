e11670686702dd8f5bd938afe456a449d993e433
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-16 22:01:18 +0530

    Half Pyramid after 180 degrees rotation

---
+++ DSA/patterns/p4.cpp
@@ -0,0 +1,23 @@
+//Halp Pyramid after 180 degrees rotation
+/*
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j<=n-i) cout<<"  ";
+            else cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
