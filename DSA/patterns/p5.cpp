c0979df79d309eed69529945030ec5f2880f7c82
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-16 22:05:32 +0530

    Half Pyramid using numbers

---
+++ DSA/patterns/p5.cpp
@@ -0,0 +1,22 @@
+//Half Pyramid Using Numbers
+/*
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
