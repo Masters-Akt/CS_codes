ac5276e42ce6e48c10421a62296bfee59dcec3fe
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-16 22:44:32 +0530

    Butterfly Pattern

---
+++ DSA/patterns/Butterfly_Pattern_p7.cpp
@@ -0,0 +1,34 @@
+//Butterfly Pattern
+/*
+For n=4
+*             *
+* *         * *
+* * *     * * *
+* * * * * * * *
+* * * * * * * *
+* * *     * * *
+* *         * *
+*             *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*n;j++){
+            if(j<=i || j>=2*n-i+1) cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=2*n;j++){
+            if(j<=i || j>=2*n-i+1) cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
