ba21c95bc47a9f90f3fe8f28f3bfafa46c881b7b
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-18 22:26:51 +0530

    Sum of Even Numbers After Queries

---
+++ leetcode_sol/985-Sum_Of_Even_Numbers_After_Queries.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> ans;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0) sum+=nums[i];
+        }
+        for(int i=0;i<queries.size();i++){
+            if((nums[queries[i][1]]+queries[i][0])%2==0){
+                if(queries[i][0]%2==0){
+                    sum+=queries[i][0];
+                }else{
+                    sum+=(nums[queries[i][1]]+queries[i][0]);
+                }
+            }else{
+                if(nums[queries[i][1]]%2==0){
+                    sum-=nums[queries[i][1]];
+                }
+            }
+            nums[queries[i][1]]+=queries[i][0];
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
