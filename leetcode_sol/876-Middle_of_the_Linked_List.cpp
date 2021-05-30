566ba72ca3ce81ae2cc8989e1bf701462777b7b4
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-30 23:12:34 +0530

    Method 2

--- leetcode_sol/876-Middle_of_the_Linked_List.cpp
+++ leetcode_sol/876-Middle_of_the_Linked_List.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Method 1
+/*
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
@@ -23,5 +26,30 @@ public:
             ans=ans->next;
         }
         return ans;
+    }
+};
+*/
+
+//Method 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slowpointer = head;
+        ListNode* fastpointer = head;
+        while(fastpointer!=NULL && fastpointer->next!=NULL){
+            slowpointer = slowpointer->next;
+            fastpointer = fastpointer->next->next;
+        }
+        return slowpointer;
     }
 };
