b11e124d6006aa3b1fba98ecfbc0ca019c0f3aea
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-05-31 14:16:20 +0530

    Recursive approach added

--- leetcode_sol/206-Reverse_Linked_List.cpp
+++ leetcode_sol/206-Reverse_Linked_List.cpp
@@ -25,3 +25,25 @@ public:
         return head;
     }
 };
+
+//Recursive method
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return p;
+    }
+};
