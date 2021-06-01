eaa90f8c0fdbd808e96a6b0e0d79dd678f0dac9d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-01 12:47:08 +0530

    Method 2 using two pointers

--- leetcode_sol/1721-Swapping_Nodes_In_A_Linked_List.cpp
+++ leetcode_sol/1721-Swapping_Nodes_In_A_Linked_List.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Method 1
+/*
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
@@ -32,6 +35,39 @@ public:
         int tem = first->val;
         first->val = second->val;
         second->val = tem;
+        return head;
+    }
+};
+*/
+
+//Method 2 using two pointers
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* first;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for(int i=1;i<k;i++){
+            fast = fast->next;
+        }
+        first = fast;
+        while(fast->next!=NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        int temp = first->val;
+        first->val = slow->val;
+        slow->val = temp;
         return head;
     }
 };
