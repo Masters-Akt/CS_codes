6aaf5702e45631b8137173fac37a232878d2ef16
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-01 12:23:58 +0530

    Added Recursive Approach

--- leetcode_sol/203-Remove_Linked_List_Elements.cpp
+++ leetcode_sol/203-Remove_Linked_List_Elements.cpp
@@ -39,3 +39,25 @@ public:
         return head;
     }
 };
+
+//Recursive Approach Method 2
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL) return head;
+        head->next = removeElements(head->next, val);
+        if(head->val==val) return head->next;
+        return head;
+    }
+};
