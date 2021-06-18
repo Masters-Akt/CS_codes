5b242452f541547abecacd1c707f49633b61371a
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-19 02:26:48 +0530

    Bad Boy

---
+++ codeforces_sol/B-Bad_Boy.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void akt(){
+    int n,m,i,j;
+	cin>>n>>m>>i>>j;
+	cout<<"1 1 "<<n<<" "<<m<<"\n";
+}
+
+signed main(){
+    // #ifndef ONLINE_JUDGE
+	// 	freopen("input.txt", "r", stdin);
+	// 	freopen("output.txt", "w", stdout);
+	// #endif
+    std::ios::sync_with_stdio(false);
+ 	cin.tie(NULL);
+    cout.tie(NULL);
+	int t;
+	cin>>t;
+	for(int test=1;test<=t;test++){
+	    akt();
+	}
+}
