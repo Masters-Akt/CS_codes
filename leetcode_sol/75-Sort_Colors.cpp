471b0775e9bcee8fb27bbcaf559af1bb2d1943ca
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-24 23:18:52 +0530

    Method 2 and 3 for Sort Colors

--- leetcode_sol/75-Sort_Colors.cpp
+++ leetcode_sol/75-Sort_Colors.cpp
@@ -1,6 +1,28 @@
+//By using sort function
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         sort(nums.begin(), nums.end());
+    }
+};
+
+//Without Using Sort Function
+
+//Method 2 = Using Counting Sort
+
+//Method 3 = Dutch National Flag Algorithm
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low++], nums[mid++]);
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid], nums[high--]);
+            }
+        }
     }
 };
