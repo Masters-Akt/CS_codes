f736f02ed405da59e0913e5a88d0c11068814404
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-25 16:44:49 +0530

    Added method 2 for set matrix zero

--- leetcode_sol/73-Set_Matrix_Zeroes.cpp
+++ leetcode_sol/73-Set_Matrix_Zeroes.cpp
@@ -1,3 +1,5 @@
+//Method 1
+//with using two arrays
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -15,6 +17,28 @@ public:
             for(int j=0;j<matrix[i].size();j++){
                 if(row[i]==1 || col[j]==1) matrix[i][j]=0;
             }
+        }
+    }
+};
+
+//Method 2
+//without using any space
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool col0 = true;
+        int row = matrix.size(), col = matrix[0].size();
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0) col0 = false;
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0) matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+        for(int i=row-1;i>=0;i--){
+            for(int j=col-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j] = 0;
+            }
+            if(!col0) matrix[i][0] = 0;
         }
     }
 };
