e4f2bdf133a9e79e41837bdc4a93305531504aca
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-25 17:26:12 +0530

    Brute Force gives TLE

--- leetcode_sol/31-Next_Permutation.cpp
+++ leetcode_sol/31-Next_Permutation.cpp
@@ -6,3 +6,26 @@ public:
         bool temp = next_permutation(nums.begin(), nums.end());
     }
 };
+
+//Method 2
+//Generate all permutations, then search for the current one and return the next one
+//Time Complexity = O(N!*N)
+//Gives TLE
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+        vector<vector<int>> store;
+        do{
+            store.push_back(temp);
+        }while(next_permutation(temp.begin(), temp.end()));
+        for(int i=0;i<store.size();i++){
+            if(store[i]==nums){
+                if(i==store.size()-1) nums = store[0];
+                else nums = store[i+1];
+                break;
+            }
+        }
+    }
+};
