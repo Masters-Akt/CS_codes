3cb92b7871ef3f8286d5ac9a5725daf18ccf42ad
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-26 14:11:58 +0530

    Search A 2D Matrix

---
+++ leetcode_sol/74-Search_A_2D_Matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = 0, j = matrix[0].size()-1;
+        while(i<matrix.size() && j>=0){
+            if(matrix[i][j]==target) return true;
+            if(matrix[i][j]>target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
