d4ad3d1f7df96bd1c84a2d86518195936e75ccce
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-26 14:31:28 +0530

    Seach a 2D Matrix

--- leetcode_sol/74-Search_A_2D_Matrix.cpp
+++ leetcode_sol/74-Search_A_2D_Matrix.cpp
@@ -1,3 +1,4 @@
+//Method 1 - pointer approach
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -6,6 +7,22 @@ public:
             if(matrix[i][j]==target) return true;
             if(matrix[i][j]>target) j--;
             else i++;
+        }
+        return false;
+    }
+};
+
+//Method 2 - Binary Search concept
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(!matrix.size()) return false;
+        int low = 0, high = matrix.size()*matrix[0].size()-1;
+        while(low<=high){
+            int mid = (low+(high-low)/2);
+            if(matrix[mid/(matrix[0].size())][mid%(matrix[0].size())] == target) return true;
+            if(matrix[(mid/matrix[0].size())][(mid%matrix[0].size())] < target) low = mid+1;
+            else high = mid-1;
         }
         return false;
     }
