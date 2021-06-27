f9d082e9a67a582b00c843436d5252b9360013dd
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-27 21:13:58 +0530

    merge sort technique for reverse pairs

--- leetcode_sol/493-Reverse_Pairs.cpp
+++ leetcode_sol/493-Reverse_Pairs.cpp
@@ -12,3 +12,39 @@ public:
         return c;
     }
 };
+
+//Method 2 - Merge Sort Implementation Technique
+class Solution {
+public:
+    int merge(vector<int> &nums, int low, int mid, int high){
+        int count = 0, j = mid+1;
+        for(int i=low;i<=mid;i++){
+            while(j<=high && nums[i]>2LL*nums[j]) j++;
+            count+=(j-(mid+1));
+        }
+        vector<int> temp;
+        int left = low, right = mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]) temp.push_back(nums[left++]);
+            else temp.push_back(nums[right++]);
+        }
+        while(left<=mid) temp.push_back(nums[left++]);
+        while(right<=high) temp.push_back(nums[right++]);
+        for(int i=low;i<=high;i++) nums[i] = temp[i-low];
+        return count;
+    }
+    
+    int mergeSort(vector<int> &nums, int low, int high){
+        if(low >= high) return 0;
+        int mid = (low+high)/2;
+        int inv = mergeSort(nums, low, mid);
+        inv+=mergeSort(nums, mid+1, high);
+        inv+=merge(nums, low, mid, high);
+        return inv;
+    }
+    
+    int reversePairs(vector<int>& nums) {
+        //Merge Sort
+        return mergeSort(nums, 0, nums.size()-1);
+    }
+};
