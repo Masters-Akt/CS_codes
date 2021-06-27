3c8400e748710892220a8c13a2466dc3bc489508
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-06-27 10:27:37 +0530

    Added DP approach for unique paths

--- leetcode_sol/62-Unique_Paths.cpp
+++ leetcode_sol/62-Unique_Paths.cpp
@@ -12,3 +12,20 @@ public:
         return countPaths(i, j, m, n);
     }
 };
+
+//Method 2 - Dynamic Problem Approach
+class Solution {
+public:
+    int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp){
+        if(i==m-1 && j==n-1) return 1;
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = countPaths(i+1, j, m, n, dp)+countPaths(i, j+1, m, n, dp);
+    }
+    
+    int uniquePaths(int m, int n) {
+        int i=0, j=0;
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return countPaths(i, j, m, n, dp);
+    }
+};
