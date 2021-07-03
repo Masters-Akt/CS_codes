2bc4a85d23932d01c7f5f5a77e6b5dd97266dcdf
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-03 21:19:16 +0530

    Odd Set

---
+++ codeforces_sol/A-Odd_Set.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void kumar_ankit(){
+    int n;
+	cin>>n;
+	int o=0, e=0;
+	for(int i=0;i<2*n;i++){
+		int x;
+		cin>>x;
+		if(x%2==0) e++;
+		else o++;
+	}
+	if(e>=n && o>=n) cout<<"YES\n";
+	else cout<<"NO\n";
+}
+
+signed main(){
+    // #ifndef ONLINE_JUDGE
+	// 	freopen("input.txt", "r", stdin);
+	// 	freopen("output.txt", "w", stdout);
+	// #endif
+    std::ios::sync_with_stdio(false);
+ 	cin.tie(NULL);
+    cout.tie(NULL);
+	int t;
+	cin>>t;
+	for(int test=1;test<=t;test++){
+	    kumar_ankit();
+	}
+}
