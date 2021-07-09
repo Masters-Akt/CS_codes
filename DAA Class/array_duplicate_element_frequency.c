b65c4d3d133406f361add4b2f29ac13e972b3346
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-09 14:09:15 +0530

    DAA Lab 1

--- DAA Class/array_duplicate_element_frequency.c
+++ DAA Class/array_duplicate_element_frequency.c
@@ -1,23 +1,16 @@
 #include<stdio.h>
 
 void printMaxRepeating(int a[], int n){
-    int m = 0;
-    int freq[n];
+    int m = 0, freq[n];
     for(int i=0;i<n;i++){
         int c = 1;
-        for(int j=i+1;j<n;j++){
-            if(a[i]==a[j]){
-                c++;
-            }
-        }
+        for(int j=i+1;j<n;j++)
+            if(a[i]==a[j]) c++;
         freq[i]=c;
     }
-    for(int i=0;i<n;i++){
-        if(freq[i]>m) m = freq[i];
-    }
-    if(m==1){
-        printf("There is no repeating element.");
-    }else{
+    for(int i=0;i<n;i++) if(freq[i]>m) m = freq[i];
+    if(m==1) printf("There is no repeating element.");
+    else{
         printf("The maximum count is %d and the repeating elements are ", m);
         for(int i=0;i<n;i++){
             if(freq[i]==m){
@@ -33,9 +26,7 @@ int main(){
     scanf("%d",&n);
     int a[100];
     printf("Enter n elements : ");
-    for(int i=0;i<n;i++){
-        scanf("%d", &a[i]);
-    }
+    for(int i=0;i<n;i++) scanf("%d", &a[i]);
     printMaxRepeating(a,n);
     return 0;
 }
