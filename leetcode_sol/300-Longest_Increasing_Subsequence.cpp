52fd3e67d2f4bc31f5e57d25acea7428f91f0e93
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-09 20:05:50 +0530

    Longest Increasing Subsequence

---
+++ leetcode_sol/300-Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(),1);
+        for(int i=1;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
