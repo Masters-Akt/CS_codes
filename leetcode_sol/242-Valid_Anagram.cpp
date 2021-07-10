59871c4d35428ea7d3d334235848b2f7277e15fb
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-10 10:41:04 +0530

    Valid Anagram updated

--- leetcode_sol/242-Valid_Anagram.cpp
+++ leetcode_sol/242-Valid_Anagram.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -6,6 +7,23 @@ public:
             a[s[i]-'a']++;
         }
         for(int i=0;i<t.size();i++){
+            a[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(a[i]!=0) return false;
+        }
+        return true;
+    }
+};
+
+//Method 2 - slight variation
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int a[26] = {0};
+        for(int i=0;i<s.size();i++){
+            a[s[i]-'a']++;
             a[t[i]-'a']--;
         }
         for(int i=0;i<26;i++){
