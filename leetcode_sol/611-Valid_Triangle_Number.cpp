f43553899ba06e0b1033c3477aaac633d958ece4
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-15 22:09:30 +0530

    Method 3 for Valid Triangle Number

--- leetcode_sol/611-Valid_Triangle_Number.cpp
+++ leetcode_sol/611-Valid_Triangle_Number.cpp
@@ -39,3 +39,21 @@ public:
         return c;
     }
 };
+
+//Method 3 - Linear Scan - O(N2)
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums){
+        if(nums.size()<3) return 0;
+        int c=0;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-2; i++){
+            int k=i+2;
+            for(int j=i+1; j<nums.size()-1 && nums[i]!=0; j++){
+                while(k<nums.size() && nums[i]+nums[j]>nums[k]) k++;
+                c+=k-j-1;
+            }
+        }
+        return c;
+    }
+};
