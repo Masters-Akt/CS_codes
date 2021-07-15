6b084fbca27c381f2c6f0c315f3e5ef078001bc5
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-15 22:04:46 +0530

    Method 2 for Valid Triangle Number

--- leetcode_sol/611-Valid_Triangle_Number.cpp
+++ leetcode_sol/611-Valid_Triangle_Number.cpp
@@ -13,3 +13,29 @@ public:
         return c;
     }
 };
+
+//Method 2 - Binary Search
+class Solution {
+public:
+    int binarySearch(vector<int> nums, int l, int r, int x){
+        while(r>=l && r<nums.size()){
+            int mid=(l+r)/2;
+            if(nums[mid]>=x) r=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+    
+    int triangleNumber(vector<int>& nums) {
+        int c=0;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-2; i++){
+            int k=i+2;
+            for(int j=i+1; j<nums.size()-1 && nums[i]!=0; j++){
+                k=binarySearch(nums, k, nums.size()-1, nums[i]+nums[j]);
+                c+=k-j-1;
+            }
+        }
+        return c;
+    }
+};
