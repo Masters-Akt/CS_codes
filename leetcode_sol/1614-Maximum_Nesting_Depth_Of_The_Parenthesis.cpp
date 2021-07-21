9e0a64c0e41cfe632b1d4c190341321f0f5d9ded
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-21 11:54:14 +0530

    Maximum Nesting Depth of the Parenthesis modified

--- leetcode_sol/1614-Maximum_Nesting_Depth_Of_The_Parenthesis.cpp
+++ leetcode_sol/1614-Maximum_Nesting_Depth_Of_The_Parenthesis.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> st;
         int c = 0, m = 0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                c++;
-                st.push(s[i]);
-            }else if(s[i]==')'){
+            if(s[i]=='(') c++;
+            else if(s[i]==')'){
                 m = max(m, c);
                 c--;
-                st.pop();
             }
         }
         return max(m, c);
