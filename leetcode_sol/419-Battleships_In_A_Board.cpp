53049451140b8a1ce23aefc2da749f92f34f35b1
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-07-27 11:04:08 +0530

    Battleships In A Board modified

--- leetcode_sol/419-Battleships_In_A_Board.cpp
+++ leetcode_sol/419-Battleships_In_A_Board.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     int countBattleships(vector<vector<char>>& board) {
@@ -21,6 +22,21 @@ public:
                         }
                     }
                 }
+            }
+        }
+        return c;
+    }
+};
+
+//Method 2 - Better Logic
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int c = 0;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='.' || (i>0 && board[i-1][j]=='X') || (j>0 && board[i][j-1]=='X')) continue;
+                c++;
             }
         }
         return c;
