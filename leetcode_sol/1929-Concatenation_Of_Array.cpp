8e30c011463aef5f264a1fa2a9d80a2f013c460d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-08-06 09:38:13 +0530

    Concatenation of Arrays

---
+++ leetcode_sol/1929-Concatenation_Of_Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans = nums;
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
