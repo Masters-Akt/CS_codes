503c1f1049274873f9d0fc1e820f917239897fe7
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-08-07 12:30:54 +0530

    Palindrome Number

--- leetcode_sol/9-Palindrome_Number.cpp
+++ leetcode_sol/9-Palindrome_Number.cpp
@@ -1,15 +1,14 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        if(x<0)
-            return false;
-        int rev=0;
-        int temp=x;
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        unsigned int rev = 0;
+        int temp = x;
         while(temp>0){
-            rev=rev*10+temp%10;
+            rev=(rev*10)+(temp%10);
             temp/=10;
         }
-        if(rev==x)
-            return true;
+        if(rev==x) return true;
         return false;
     }
-}
+};
