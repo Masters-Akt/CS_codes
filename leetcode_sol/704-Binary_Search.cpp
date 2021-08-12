9e9b859e80cb54e380a01f7bad9a94f9d34fab18
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-08-12 10:43:50 +0530

    Binary Search recursive

--- leetcode_sol/704-Binary_Search.cpp
+++ leetcode_sol/704-Binary_Search.cpp
@@ -1,3 +1,4 @@
+//Iterative way
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -9,5 +10,21 @@ public:
             else l = mid+1;
         }
         return -1;
+    }
+};
+
+//Recursive way
+class Solution {
+public:
+    int bsearch(vector<int> nums, int l, int r, int t){
+        if(l>r) return -1;
+        int mid = (l+r)/2;
+        if(nums[mid]==t) return mid;
+        if(t<nums[mid]) return bsearch(nums, l, mid-1, t);
+        return bsearch(nums, mid+1, r, t);   
+    }
+    
+    int search(vector<int>& nums, int target) {
+        return bsearch(nums, 0, nums.size()-1, target);
     }
 };
