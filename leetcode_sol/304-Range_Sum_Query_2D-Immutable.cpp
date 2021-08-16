4e74f0969ecdf3afb52d9585ee06e093b99084e9
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-08-16 13:35:20 +0530

    Range Sum Query Immutable TLE

---
+++ leetcode_sol/304-Range_Sum_Query_2D-Immutable.cpp
@@ -0,0 +1,19 @@
+//Method 1 - Brute Force - O(N2) - TLE
+
+class NumMatrix {
+public:
+    vector<vector<int>> m;
+    NumMatrix(vector<vector<int>>& matrix) {
+        m = matrix;
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for(int i=row1;i<=row2;i++){
+            for(int j=col1;j<=col2;j++){
+                sum+=m[i][j];
+            }
+        }
+        return sum;
+    }
+};
