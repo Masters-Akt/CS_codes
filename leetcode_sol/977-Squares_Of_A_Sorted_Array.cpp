9e041aa6d55754ea8dd805c31d598df10d9abc8d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-09-11 16:13:24 +0530

    Squares of a sorted array m-2

--- leetcode_sol/977-Squares_Of_A_Sorted_Array.cpp
+++ leetcode_sol/977-Squares_Of_A_Sorted_Array.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
@@ -6,6 +7,26 @@ public:
             ans.push_back(nums[i]*nums[i]);
         }
         sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//Method - 2 -> Two pointers
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int start = 0, end = nums.size()-1;
+        int index = nums.size()-1;
+        while(start<=end){
+            if(nums[start]*nums[start]>nums[end]*nums[end]){
+                ans[index--] = nums[start]*nums[start];
+                start++;
+            }else{
+                ans[index--] = nums[end]*nums[end];
+                end--;
+            }
+        }
         return ans;
     }
 };
