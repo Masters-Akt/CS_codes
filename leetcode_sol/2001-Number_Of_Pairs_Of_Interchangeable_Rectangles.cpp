523a79a5a324a327dfb6c74af0f74e92c3def9cc
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-09-15 12:26:23 +0530

    Number Of Pairs Of Interchangeable Rectangles

---
+++ leetcode_sol/2001-Number_Of_Pairs_Of_Interchangeable_Rectangles.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        int n = rectangles.size();
+        vector<double> ratio(n, 0.0);
+        for(int i=0;i<n;i++){
+            ratio[i] = (double)(rectangles[i][0])/(double)(rectangles[i][1]);
+        }
+        sort(ratio.begin(), ratio.end());
+        for(int i=0;i<n;i++) cout<<ratio[i]<<" ";
+        cout<<endl;
+        long long c = 0, temp = 1;
+        for(int i=1;i<n;i++){
+            if(ratio[i]==ratio[i-1]) temp++;
+            else{
+                c+=((temp*(temp-1))/2);
+                temp = 1;
+            }
+        }
+        if(temp!=0){
+            c+=((temp*(temp-1))/2);
+        }
+        return c;
+    }
+};
