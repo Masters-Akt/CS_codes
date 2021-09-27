446e96a50c8db020c74d72e9a8a459168fbc5ae3
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-09-27 22:54:45 +0530

    Add Two Numbers

--- leetcode_sol/2-Add_Two_Numbers.cpp
+++ leetcode_sol/2-Add_Two_Numbers.cpp
@@ -26,3 +26,28 @@ public:
         return ans.next;
     }
 };
+
+//Method 2 - Recursive
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(!l1 && !l2) return NULL;
+        int carry = (l1?l1->val:0)+(l2?l2->val:0);
+        ListNode *newHead = new ListNode(carry%10), *next = l1?l1->next:NULL;
+        carry/=10;
+        if(next) next->val+=carry;
+        else if(carry) next = new ListNode(carry);
+        newHead->next = addTwoNumbers(l2?l2->next:NULL, next);
+        return newHead;
+    }
+};
