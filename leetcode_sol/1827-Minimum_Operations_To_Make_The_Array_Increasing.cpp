4234678837db45201867876134a4d263a49937b8
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-10-12 12:18:27 +0530

    Minimum Operations To Make The Array Increasing

--- leetcode_sol/1827-Minimum_Operations_To_Make_The_Array_Increasing.cpp
+++ leetcode_sol/1827-Minimum_Operations_To_Make_The_Array_Increasing.cpp
@@ -1,3 +1,4 @@
+//Method - 1 - By altering The array
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
@@ -7,6 +8,20 @@ public:
                 ans+=(nums[i-1]-nums[i]+1);
                 nums[i]=nums[i-1]+1;
             }
+        }
+        return ans;
+    }
+};
+//Method - 2 - By not altering The array
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0, last = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<=last){
+                ans+=(last-nums[i]+1);
+                last++;
+            }else last = nums[i];
         }
         return ans;
     }
