95ac861afa915692d9af62fa9a7e437e251b4675
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-10-13 12:16:18 +0530

    K Closest Points To Origin

--- leetcode_sol/973-K_Closest_Points_To_Origin.cpp
+++ leetcode_sol/973-K_Closest_Points_To_Origin.cpp
@@ -1,3 +1,4 @@
+//Method - 1 - O(NlogN)
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
@@ -11,6 +12,24 @@ public:
         sort(store.begin(), store.end());
         for(int i=0; i<k; i++){
             ans.push_back({store[i].second.first, store[i].second.second});
+        }
+        return ans;
+    }
+};
+
+//Method - 2 - Using Multimap
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> ans;
+        multimap<double, int> m;
+        for(int i=0;i<points.size();i++){
+            m.insert({points[i][0]*points[i][0] + points[i][1]*points[i][1], i});
+        }
+        auto it = m.begin();
+        while(k--){
+            ans.push_back(points[it->second]);
+            it++;
         }
         return ans;
     }
