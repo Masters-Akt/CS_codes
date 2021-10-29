28a5801eda4b280363409106950e9b6468aa6907
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-10-29 23:44:36 +0530

    Min Stack

--- leetcode_sol/155-Min_Stack.cpp
+++ leetcode_sol/155-Min_Stack.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class MinStack {
 public:
     vector<pair<int, int>> mystack;
@@ -20,6 +21,51 @@ public:
     
     int getMin() {
         return mystack.back().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+//Method 2 - Mathematics involved
+class MinStack {
+public:
+    stack<long long> s;
+    long long m;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(s.empty()){
+            s.push(val);
+            m = val;
+        }else if(val>=m){
+            s.push(val);
+        }else{
+            s.push((long)val*2-m);
+            m = val;
+        }
+    }
+    
+    void pop() {
+        if(s.top()<m) m = 2*m-s.top();
+        s.pop();
+    }
+    
+    int top() {
+        if(s.top()<m) return m;
+        return s.top();
+    }
+    
+    int getMin() {
+        return m;
     }
 };
 
