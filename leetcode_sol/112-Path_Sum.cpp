da88e92893f17cec4f4480a4b15067e18bba10ba
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-11-03 23:01:24 +0530

    Path Sum

--- leetcode_sol/112-Path_Sum.cpp
+++ leetcode_sol/112-Path_Sum.cpp
@@ -39,3 +39,24 @@ public:
         return hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val);
     }
 };
+
+//Method 3 - Recursion Once Again
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        if(!root->left && !root->right) return targetSum==root->val;
+        return hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val);
+    }
+};
