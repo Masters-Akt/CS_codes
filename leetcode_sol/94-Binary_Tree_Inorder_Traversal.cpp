363f57acac64ac5cb319213734002501b5cc1732
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-11-04 01:44:37 +0530

    Binary Tree Inorder Traversal

--- leetcode_sol/94-Binary_Tree_Inorder_Traversal.cpp
+++ leetcode_sol/94-Binary_Tree_Inorder_Traversal.cpp
@@ -1,3 +1,4 @@
+//Method 1 - Recursive
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -21,6 +22,40 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         inorder(root, ans);
+        return ans;
+    }
+};
+
+//Method 2 - Iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> s;
+        TreeNode* temp = root;
+        while(1){
+            if(temp!=NULL){
+                s.push(temp);
+                temp = temp->left;
+            }else{
+                if(s.empty()) break;
+                temp = s.top();
+                s.pop();
+                ans.push_back(temp->val);
+                temp = temp->right;
+            }
+        }
         return ans;
     }
 };
