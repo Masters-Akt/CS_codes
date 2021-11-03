50e7cd29b882ea5e0ccdbedc60b3a04a270a4ec9
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-11-04 00:50:06 +0530

    Binary Tree Preorder traversal

--- leetcode_sol/144-Binary_Tree_Preorder_Traversal.cpp
+++ leetcode_sol/144-Binary_Tree_Preorder_Traversal.cpp
@@ -1,3 +1,4 @@
+//Method 1 - Recursion
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -21,6 +22,36 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
         preorder(root, ans);
+        return ans;
+    }
+};
+
+//Method 2 - Iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* temp = s.top();
+            s.pop();
+            ans.push_back(temp->val);
+            if(temp->right) s.push(temp->right);
+            if(temp->left) s.push(temp->left);
+        }
         return ans;
     }
 };
