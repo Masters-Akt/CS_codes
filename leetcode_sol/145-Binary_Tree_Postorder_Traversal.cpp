9856de4c1530d7fffeea197b7a61622ada1d6458
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-11-04 09:12:01 +0530

    Binary Tree Postorder Traversal

--- leetcode_sol/145-Binary_Tree_Postorder_Traversal.cpp
+++ leetcode_sol/145-Binary_Tree_Postorder_Traversal.cpp
@@ -1,3 +1,4 @@
+//Method 1 - Recursion
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -21,6 +22,40 @@ public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
         helper(root, ans);
+        return ans;
+    }
+};
+
+//Method 2 - Iterative using 2 Stacks
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        stack<TreeNode*> s1, s2;
+        s1.push(root);
+        while(!s1.empty()){
+            TreeNode* temp = s1.top();
+            s1.pop();
+            s2.push(temp);
+            if(temp->left) s1.push(temp->left);
+            if(temp->right) s1.push(temp->right);
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
         return ans;
     }
 };
