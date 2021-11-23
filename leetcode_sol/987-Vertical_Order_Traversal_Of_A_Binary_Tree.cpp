3101451f3b7aada6858018114b6c7668b032afab
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-11-23 22:43:50 +0530

    Vertical Order traversal Of A Binary Tree

---
+++ leetcode_sol/987-Vertical_Order_Traversal_Of_A_Binary_Tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            TreeNode* node = p.first;
+            int x = p.second.first, y = p.second.second;
+            nodes[x][y].insert(node->val);
+            if(node->left) q.push({node->left, {x-1, y+1}});
+            if(node->right) q.push({node->right, {x+1, y+1}});
+        }
+        for(auto p: nodes){
+            vector<int> temp;
+            for(auto r: p.second){
+                temp.insert(temp.end(), r.second.begin(), r.second.end());
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
