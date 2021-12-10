f9a749c39ab68f914746204e0a0e078b0d0d7ed2
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-10 22:42:25 +0530

    House Robber Memoization DP

--- leetcode_sol/198-House_Robber.cpp
+++ leetcode_sol/198-House_Robber.cpp
@@ -1,4 +1,4 @@
-//Using Recursion - Take or not take - TLE
+//Using Recursion - Take or not take - TLE - O(2^n)
 class Solution {
 private:
     int recfunc(int ind, vector<int>& nums){
@@ -12,5 +12,24 @@ private:
 public:
     int rob(vector<int>& nums) {
         return recfunc(0, nums);
+    }
+};
+
+//Memoization DP
+class Solution {
+private:
+    int recfunc(int ind, vector<int>& nums, vector<int>& dp){
+        if(ind>=nums.size()) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        //steal
+        int steal = nums[ind] + recfunc(ind+2, nums, dp);
+        //not steal
+        int notsteal = recfunc(ind+1, nums, dp);
+        return dp[ind] = max(steal, notsteal);
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return recfunc(0, nums, dp);
     }
 };
