571fe561b681bd884c0923d920891a2d94d5c0ae
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-10 22:49:02 +0530

    House Robber Iterative DP

--- leetcode_sol/198-House_Robber.cpp
+++ leetcode_sol/198-House_Robber.cpp
@@ -33,3 +33,17 @@ public:
         return recfunc(0, nums, dp);
     }
 };
+
+//Iterative DP
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+2, 0);
+        dp[n] = dp[n+1] = 0;
+        for(int i=n-1;i>=0;i--){
+            dp[i] = max(nums[i]+dp[i+2], dp[i+1]);
+        }
+        return dp[0];
+    }
+};
