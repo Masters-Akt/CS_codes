a0b700753ac0de842803b2899ab4b3ef155b46b0
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-11 11:04:25 +0530

    Minimum Cost to move chips to the same position

--- leetcode_sol/1217-Minimum_Cost_To_Move_Chips_To_The_Same_Position.cpp
+++ leetcode_sol/1217-Minimum_Cost_To_Move_Chips_To_The_Same_Position.cpp
@@ -9,3 +9,16 @@ public:
         return min(countodd, counteven);
     }
 };
+
+//Another way of writing the same thing
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+       int countodd = 0, counteven = 0;
+        for(auto &it: position){
+            countodd+=(it&1);
+            counteven+=(!(it&1));
+        }
+        return min(countodd, counteven);
+    }
+};
