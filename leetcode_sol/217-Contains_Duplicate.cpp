9b116bb1e67b4e843236e621208f7235fff054d6
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-16 23:06:32 +0530

    contains duplicates

--- leetcode_sol/217-Contains_Duplicate.cpp
+++ leetcode_sol/217-Contains_Duplicate.cpp
@@ -1,9 +1,23 @@
+//M1
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         for(int i=1;i<nums.size();i++){
             if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    }
+};
+
+//M2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, int> m;
+        for(int i=0;i<nums.size();i++){
+            if(m.find(nums[i])!=m.end()) return true;
+            else m[nums[i]] = 1;
         }
         return false;
     }
