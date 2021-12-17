5b3540be2aa904f3d0b33ba21b41d51814b9d413
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-18 00:23:08 +0530

    Top Down DP for Min Cost Climbing stairs

--- leetcode_sol/746-Min_Cost_Climbing_Stairs.cpp
+++ leetcode_sol/746-Min_Cost_Climbing_Stairs.cpp
@@ -11,3 +11,19 @@ public:
         return min(rec(cost, cost.size()-1), rec(cost, cost.size()-2));
     }
 };
+
+//M2 - Top Down DP - Recursion+Memoization
+class Solution {
+private:
+    int rec(vector<int> cost ,int n, vector<int>& dp){
+        if(n<0) return 0;
+        if(n==0 || n==1) return cost[n];
+        if(dp[n]) return dp[n];
+        return dp[n] = cost[n]+min(rec(cost, n-1, dp), rec(cost, n-2, dp));
+    }
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size());
+        return min(rec(cost, cost.size()-1, dp), rec(cost, cost.size()-2, dp));
+    }
+};
