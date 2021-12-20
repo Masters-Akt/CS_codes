66d13859c3234397b39a1a333109748531b6a141
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-20 23:45:35 +0530

    Sum Of Nodes With Even Valued Grandparent

--- leetcode_sol/1315-Sum_Of_Nodes_With_Even-Valued_Grandparent.cpp
+++ leetcode_sol/1315-Sum_Of_Nodes_With_Even-Valued_Grandparent.cpp
@@ -1,3 +1,4 @@
+//Method 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -31,6 +32,30 @@ public:
         if(root==NULL) return 0;
         int ans = 0;
         solve(root, ans);
+        return ans;
+    }
+};
+
+//Method 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumEvenGrandparent(TreeNode* root, int parent=-1, int grandparent=-1) {
+        if(!root) return 0;
+        int ans = 0;
+        ans+=sumEvenGrandparent(root->left, root->val, parent);
+        ans+=sumEvenGrandparent(root->right, root->val, parent);
+        if(grandparent!=-1 && !(grandparent&1)) ans+=root->val;
         return ans;
     }
 };
