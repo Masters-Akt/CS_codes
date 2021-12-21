18dee8a1d2571d3d70a8eb3d1940f8589c484298
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-21 19:52:16 +0530

    Rotate Array

--- leetcode_sol/189-Rotate_Array.cpp
+++ leetcode_sol/189-Rotate_Array.cpp
@@ -6,3 +6,22 @@ public:
         std::rotate(nums.begin(), nums.begin()+nums.size()-k, nums.end());
     }
 };
+
+//Method 2
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k>nums.size()) k=k%nums.size();
+        vector<int> temp;
+        int i = nums.size()-1;
+        while(k--){
+            temp.push_back(nums[i--]);
+            nums.pop_back();
+        }
+        reverse(temp.begin(), temp.end());
+        for(int i=0;i<nums.size();i++){
+            temp.push_back(nums[i]);
+        }
+        nums = temp;
+    }
+};
