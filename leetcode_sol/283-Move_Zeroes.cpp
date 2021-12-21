6483a614f9997c42ea17bb25833c985227068929
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-21 19:57:02 +0530

    Move Zeroes

--- leetcode_sol/283-Move_Zeroes.cpp
+++ leetcode_sol/283-Move_Zeroes.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -12,5 +13,22 @@ public:
         while(count--){
             nums.push_back(0);
         }
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> temp;
+        int c = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) c++;
+            else temp.push_back(nums[i]);
+        }
+        while(c--){
+            temp.push_back(0);
+        }
+        nums = temp;
     }
 };
