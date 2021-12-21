f01635ae03bdbfc22c1fc1448fb9aaeba198b367
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-21 19:41:05 +0530

    Squares Of A Sorted Array

--- leetcode_sol/977-Squares_Of_A_Sorted_Array.cpp
+++ leetcode_sol/977-Squares_Of_A_Sorted_Array.cpp
@@ -30,3 +30,14 @@ public:
         return ans;
     }
 };
+
+//Method 3 - Two Pointers same approach but a little short
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        n-=(n&-n); //removing 1st bit and checking if it is 0 or not
+        if(n==0) return true;
+        return false;
+    }
+};
