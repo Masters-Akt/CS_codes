5e8adfed2ac2ab2d0eda7d1cd923e034a2157c4d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-26 23:08:42 +0530

    Remove Zero Sum Consecutive Nodes From Linked List

--- leetcode_sol/1171-Remove_Zero_Sum_Consecutive_Nodes_From_Linked_List.cpp
+++ leetcode_sol/1171-Remove_Zero_Sum_Consecutive_Nodes_From_Linked_List.cpp
@@ -18,33 +18,33 @@ public:
         unordered_map<int, int> m; //storing possible sum
         m[0] = 1;
         while(temp){
-            if(temp->val==0){
+            if(temp->val==0){ //ignorimg 0 values
                 temp = temp->next;
                 continue;
             }
             if(!st.empty()){
                 int tempsum = sum+temp->val;
-                if(m[tempsum]==1){
-                    while(sum!=tempsum && !st.empty()){
+                if(m[tempsum]==1){ //if already found in map
+                    while(sum!=tempsum && !st.empty()){ //keep popping and checking
                         m[sum] = 0; //removing not useful value
                         sum-=(st.top()->val);
                         st.pop();
                     }
                 }else{
-                    sum = tempsum;
-                    m[sum] = 1;
-                    st.top()->next = temp;
-                    st.push(temp);
+                    sum = tempsum; //update sum
+                    m[sum] = 1; //enter into map
+                    st.top()->next = temp; 
+                    st.push(temp); //store in stack
                 }
             }else{
-                st.push(temp);
-                sum+=temp->val;
-                m[sum] = 1;
+                st.push(temp); //store it
+                sum+=temp->val; //update sum
+                m[sum] = 1; //add in map
                 ans = temp;
             }
             temp = temp->next;
         }
-        if(!st.empty()) st.top()->next = NULL;
+        if(!st.empty()) st.top()->next = NULL; 
         else ans = NULL;
         return ans;
     }
