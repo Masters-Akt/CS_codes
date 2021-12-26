5a74ba5375a2dcb95e40bafd365f5951a35cff74
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-27 02:52:45 +0530

    Intervals Between Identical Elements

--- leetcode_sol/2121-Intervals_Between_Identical_Elements.cpp
+++ leetcode_sol/2121-Intervals_Between_Identical_Elements.cpp
@@ -1,3 +1,4 @@
+//M-1 - Brute Force - O(N2) - TLE
 class Solution {
 public:
     vector<long long> getDistances(vector<int>& arr) {
@@ -13,6 +14,38 @@ public:
                 }
                 ans[ind[j]] = sum;
             }
+        }
+        return ans;
+    }
+};
+
+//M-2 - Optimized
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& arr) {
+        //Formula Used
+        /*
+        Take example A, B, C, D, E
+        calc for C :
+        = |C-A|+|C-B|+|D-C|+|E-C|
+        Hence first two can be opened as 2*C-(A+B) and later two as (D+E)-2*C
+        
+        Hence prefix sum concept to be used from both side of the array and final answer can be achieved
+        */
+        unordered_map<int, long long> m; //keeping count entries
+        unordered_map<int, long long> sum; //keepng prefix sum;
+        vector<long long> ans(arr.size(), 0);
+        for(int i=0;i<arr.size();i++){
+            ans[i]+=(m[arr[i]]*i*1ll-sum[arr[i]]);
+            m[arr[i]]++;
+            sum[arr[i]]+=i;
+        }
+        m.clear();
+        sum.clear();
+        for(int i=arr.size()-1;i>=0;i--){
+            ans[i]+=(sum[arr[i]]-(m[arr[i]]*i*1ll));
+            m[arr[i]]++;
+            sum[arr[i]]+=i;
         }
         return ans;
     }
