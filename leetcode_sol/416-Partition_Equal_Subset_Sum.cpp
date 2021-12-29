3464f14c421a43627dd8d8443a4b4c56d464e542
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2021-12-30 01:25:35 +0530

    Partition Equal Subset Sum

--- leetcode_sol/416-Partition_Equal_Subset_Sum.cpp
+++ leetcode_sol/416-Partition_Equal_Subset_Sum.cpp
@@ -1,3 +1,31 @@
+//Better way - Using Subset Sum function
+class Solution {
+private:
+    bool subsetSum(vector<int>& nums, int sum){
+        vector<vector<bool>> dp(nums.size()+1, vector<bool>(sum+1));
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[i].size();j++){
+                if(i==0 || j==0){
+                    if(i==0) dp[i][j] = false;
+                    if(j==0) dp[i][j] = true;
+                    continue;
+                }
+                if(nums[i-1]<=j) dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[nums.size()][sum];
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int x: nums) sum+=x;
+        if(sum%2) return false;
+        return subsetSum(nums, sum/2);
+    }
+};
+
+//Same code
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
