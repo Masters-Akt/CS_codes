82b5adfd51f43388103054b35a95b57a39800d74
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-02 23:38:51 +0530

    Nth Tribonacci Number

--- leetcode_sol/1137-N-th_Tribonacci_Number.cpp
+++ leetcode_sol/1137-N-th_Tribonacci_Number.cpp
@@ -1,3 +1,4 @@
+//Dp
 class Solution {
 public:
     int tribonacci(int n) {
@@ -6,5 +7,21 @@ public:
             dp[i%3] = dp[0]+dp[1]+dp[2];
         }
         return dp[n%3];
+    }
+};
+
+//Another way for DP
+class Solution {
+private:
+    int solve(vector<int>& dp, int n){
+        if(n==0 || n==1 || n==2) return dp[n];
+        if(dp[n]!=0) return dp[n];
+        return dp[n] = solve(dp, n-1)+solve(dp, n-2)+solve(dp, n-3);
+    }
+public:
+    int tribonacci(int n) {
+        vector<int> dp(38, 0);
+        dp[1] = dp[2] = 1;
+        return solve(dp, n);
     }
 };
