024c3d60cc72c8827b7b65ed12f19e5a92aa785e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-04 19:30:21 +0530

    Subarray Product Less Tha K

--- leetcode_sol/713-Subarray_Product_Less_Than_K.cpp
+++ leetcode_sol/713-Subarray_Product_Less_Than_K.cpp
@@ -1,3 +1,4 @@
+//Sliding Window
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
@@ -11,6 +12,24 @@ public:
                 i++;
             }
             ans+=(j-i+1);
+        }
+        return ans;
+    }
+};
+
+//Cleaner code for sliding window approach
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<=1) return 0;
+        int prod = 1, ans = 0, left_pointer = 0;
+        for(int right_pointer=0; right_pointer<nums.size();right_pointer++){
+            prod*=nums[right_pointer];
+            while(prod>=k){
+                prod/=nums[left_pointer];
+                left_pointer++;
+            }
+            ans+=(right_pointer-left_pointer+1);
         }
         return ans;
     }
