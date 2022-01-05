d12f4ad29b7adb85e3a378b071957a38e00f1942
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-06 00:28:14 +0530

    Palindrome Partitioning

--- leetcode_sol/131-Palindrome_Partitioning.cpp
+++ leetcode_sol/131-Palindrome_Partitioning.cpp
@@ -1,4 +1,4 @@
-//M1 - DFS+Backtracking - TC = O(N.2^N)
+//M1 - DFS+Backtracking - TC = O(N.2^N) - SC = O(N)
 class Solution {
 private:
     bool check_palindrome(string& s, int start, int end){
@@ -23,5 +23,70 @@ public:
         vector<string> current;
         dfs(s, 0, ans, current);
         return ans;
+    }
+};
+
+//M2 - DP without backtracking - TC = O(N3)
+//Approach: Start with last index of string, calc ans for it and store it.
+//We create substring with each j and check if it is palindrome, we already know the ans for rest of substrings stored in dp array, hence no backtracking
+class Solution {
+private:
+    bool check_palindrome(string& s){
+        int start = 0, end = s.size()-1;
+        while(start<=end){
+            if(s[start++]!=s[end--]) return false;
+        }
+        return true;
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<vector<string>>> dp(s.size()+1);
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=i;j<s.size();j++){
+                string temp_substr = s.substr(i, j-i+1);
+                if(check_palindrome(temp_substr)){
+                    vector<vector<string>> temp = dp[j+1];
+                    if(temp.size()!=0){
+                        for(int k=0;k<temp.size();k++){
+                            temp[k].insert(temp[k].begin(), temp_substr);
+                            dp[i].push_back(temp[k]);
+                        }
+                    }else dp[i].push_back({temp_substr});
+                }
+            }
+        }
+        return dp[0];
+    }
+};
+
+//Same approach dp without backtracking but not using insert, instead reversing the required ans
+class Solution {
+private:
+    bool check_palindrome(string& s){
+        int start = 0, end = s.size()-1;
+        while(start<=end){
+            if(s[start++]!=s[end--]) return false;
+        }
+        return true;
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<vector<string>>> dp(s.size()+1);
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=i;j<s.size();j++){
+                string temp_substr = s.substr(i, j-i+1);
+                if(check_palindrome(temp_substr)){
+                    vector<vector<string>> temp = dp[j+1];
+                    if(temp.size()!=0){
+                        for(int k=0;k<temp.size();k++){
+                            temp[k].push_back(temp_substr);
+                            dp[i].push_back(temp[k]);
+                        }
+                    }else dp[i].push_back({temp_substr});
+                }
+            }
+        }
+        for(int i=0;i<dp[0].size();i++) reverse(dp[0][i].begin(), dp[0][i].end());
+        return dp[0];
     }
 };
