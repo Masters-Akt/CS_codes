6c3b2dd1613b72825657ff7a6e3ff19d152d4ad5
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-09 18:48:40 +0530

    Climbing Stairs

--- leetcode_sol/70-Climbing_Stairs.cpp
+++ leetcode_sol/70-Climbing_Stairs.cpp
@@ -1,3 +1,13 @@
+//Recursion - TLE
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1) return 1;
+        return climbStairs(n-1)+climbStairs(n-2);
+    }
+};
+
+//Method optimized
 class Solution {
 public:
     int climbStairs(int n) {
