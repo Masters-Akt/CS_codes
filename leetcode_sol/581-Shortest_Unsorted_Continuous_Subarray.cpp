0b703ebcd07628978ef3ff3e006a0b3ab2b70144
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-10 00:44:10 +0530

    Shortest Unsorted Continuous Subarray

--- leetcode_sol/581-Shortest_Unsorted_Continuous_Subarray.cpp
+++ leetcode_sol/581-Shortest_Unsorted_Continuous_Subarray.cpp
@@ -15,3 +15,18 @@ public:
         return 0;
     }
 };
+
+//O(N)
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int beg=-1, end=-2, leftmax=nums[0], rightmin=nums[nums.size()-1];
+        for(int i=1;i<nums.size();i++){
+            leftmax = max(leftmax, nums[i]);
+            rightmin = min(rightmin, nums[nums.size()-i-1]);
+            if(nums[i]<leftmax) end = i;
+            if(nums[nums.size()-i-1]>rightmin) beg = nums.size()-i-1;
+        }
+        return end-beg+1;
+    }
+};
