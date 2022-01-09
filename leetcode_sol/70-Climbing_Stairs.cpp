ce38f14608be9bef04fb145649e8565930a03ed0
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-09 18:53:33 +0530

    Climbing Stairs

--- leetcode_sol/70-Climbing_Stairs.cpp
+++ leetcode_sol/70-Climbing_Stairs.cpp
@@ -7,7 +7,7 @@ public:
     }
 };
 
-//Memoization
+//Memoization - Top Down DP
 class Solution {
 private:
     int solve(int n, vector<int>& dp){
@@ -20,6 +20,19 @@ public:
         dp[0]=1;
         dp[1]=1;
         return solve(n, dp);
+    }
+};
+
+//Bottom Up DP - Tabulation
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(46, -1);
+        dp[0] = dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
 
