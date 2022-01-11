42be66b17e7a0d90dce2edb1bb96ff1c63b61e80
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-11 19:23:34 +0530

    Reverse Integer

--- leetcode_sol/7-Reverse_Integer.cpp
+++ leetcode_sol/7-Reverse_Integer.cpp
@@ -1,3 +1,4 @@
+//Method
 class Solution {
 public:
     int reverse(int x) {
@@ -7,5 +8,19 @@ public:
             x/=10;
         }
         return (res<INT_MIN || res>INT_MAX)?0:res;
+    }
+};
+
+//Same method without using long long
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x){
+            if(ans>INT_MAX/10 || ans<INT_MIN/10) return 0;
+            ans = ans*10+x%10;
+            x/=10;
+        }
+        return ans;
     }
 };
