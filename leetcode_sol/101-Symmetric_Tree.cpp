54d9beda25279b1b5482ae6920e47e7672d30004
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-19 20:06:05 +0530

    Symmetric Tree

--- leetcode_sol/101-Symmetric_Tree.cpp
+++ leetcode_sol/101-Symmetric_Tree.cpp
@@ -1,3 +1,29 @@
+//Using Recursion
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool solve(TreeNode* l, TreeNode* r){
+        if(!l || !r) return l==r;
+        if(l->val!=r->val) return false;
+        return solve(l->left, r->right) && solve(l->right, r->left);
+    }
+public:
+    bool isSymmetric(TreeNode* root) {
+        return !root || solve(root->left, root->right);
+    }
+};
+
+//Using Queue
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
