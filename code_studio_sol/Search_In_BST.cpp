3512d07a6895ff0e2852ad514e6c8146d169ca22
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-22 17:40:27 +0530

    Search In BST

---
+++ code_studio_sol/Search_In_BST.cpp
@@ -0,0 +1,24 @@
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    while(root!=NULL && x!=root->data){
+        root = (x<root->data)?root->left:root->right;
+    }
+    return root;
+}
