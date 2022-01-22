6a3296c85a59c436ce1db70faf5eda217752ccbd
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-22 17:56:22 +0530

    Floor in BST

---
+++ code_studio_sol/Floor_In_BST.cpp
@@ -0,0 +1,35 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int X)
+{
+    // Write your code here.
+    int floor = -1;
+    while(root){
+        if(root->val==X) return X;
+        if(X>root->val){
+            floor = root->val;
+            root = root->right;
+        }else{
+            root = root->left;
+        }
+    }
+    return floor;
+}
