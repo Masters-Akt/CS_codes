5062ef6333e9c23daa49aee977fc7e673ffe7192
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-23 15:33:36 +0530

    Two Sum IV - Input Is A BST

---
+++ leetcode_sol/653-Two_Sum_IV-Input_Is_A_BST.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator{
+private:
+    stack<TreeNode*> myStack;
+    bool reverse = true; //reverse->true=before, reverse->false=next
+    
+    void pushAll(TreeNode* node){
+        while(node){
+            myStack.push(node);
+            if(reverse) node = node->right;
+            else node = node->left;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root, bool isReverse){
+        reverse = isReverse;
+        pushAll(root);
+    }
+    bool hasNext(){
+        return !myStack.empty();
+    }
+    int next(){
+        TreeNode* temp = myStack.top();
+        myStack.pop();
+        if(reverse) pushAll(temp->left);
+        else pushAll(temp->right);
+        return temp->val;
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+        BSTIterator l(root, false); //next
+        BSTIterator r(root, true); //before
+        int i = l.next(), j=r.next();
+        while(i<j){
+            if(i+j==k) return true;
+            else if(i+j<k) i = l.next();
+            else j = r.next();
+        }
+        return false;
+    }
+};
