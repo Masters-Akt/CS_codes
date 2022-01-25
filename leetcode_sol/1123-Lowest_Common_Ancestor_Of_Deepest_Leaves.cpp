55de7fa84f044aa16767df667ad6ca3e742c43f6
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-25 23:14:38 +0530

    Lowest Common Ancestor Of Deepest Leaves

---
+++ leetcode_sol/1123-Lowest_Common_Ancestor_Of_Deepest_Leaves.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(!root || root==p || root==q) return root;
+        TreeNode* left = lca(root->left, p, q);
+        TreeNode* right = lca(root->right, p, q);
+        if(!left) return right;
+        if(!right) return left;
+        return root;
+    }
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if(!root || (!root->left && !root->right)) return root;
+        TreeNode* leftmost = NULL;
+        TreeNode* rightmost = NULL;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int s = q.size();
+            for(int i=0;i<s;i++){
+                TreeNode* x = q.front();
+                q.pop();
+                if(i==0) leftmost = x;
+                if(i==s-1) rightmost = x;
+                if(x->left) q.push(x->left);
+                if(x->right) q.push(x->right);
+            }
+        }
+        return lca(root, leftmost, rightmost);
+    }
+};
