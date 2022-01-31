6ef88e14733923332d544e0490eebdc7ffcfb2f7
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-31 20:19:22 +0530

    Frog Jump Tabulation DP

--- code_studio_sol/Frog_Jump.cpp
+++ code_studio_sol/Frog_Jump.cpp
@@ -29,3 +29,19 @@ int frogJump(int n, vector<int> &heights)
     vector<int> dp(n+1, -1);
     return solve(n-1, heights, dp);
 }
+
+//M3 - Tabulation / Bottom Up DP
+#include<limits.h>
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int> dp(n, -1);
+    dp[0] = 0;
+    for(int i=1;i<n;i++){
+        int fs = dp[i-1]+abs(heights[i]-heights[i-1]);
+        int ss = INT_MAX;
+        if(i>1) ss = dp[i-2]+abs(heights[i]-heights[i-2]);
+        dp[i] = min(fs, ss);
+    }
+    return dp[n-1];
+}
