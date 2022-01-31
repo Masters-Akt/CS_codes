416b083308174935265ca44cf82bb903002b8e85
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-31 20:16:10 +0530

    Frog Jump Memoization

--- code_studio_sol/Frog_Jump.cpp
+++ code_studio_sol/Frog_Jump.cpp
@@ -12,3 +12,20 @@ int frogJump(int n, vector<int> &heights)
     // Write your code here.
     return solve(n-1, heights);
 }
+
+//M2 - Recursion+Memoization
+#include<limits.h>
+int solve(int index, vector<int>& heights, vector<int>& dp){
+    if(index==0) return 0;
+    if(dp[index]!=-1) return dp[index];
+    int left = solve(index-1, heights, dp)+abs(heights[index]-heights[index-1]);
+    int right = INT_MAX;
+    if(index>1) right = solve(index-2, heights, dp)+abs(heights[index]-heights[index-2]);
+    return dp[index] = min(left, right);
+}
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int> dp(n+1, -1);
+    return solve(n-1, heights, dp);
+}
