8bccfb1a5b4ee30fc849927498540737c26d556b
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-01-31 20:23:39 +0530

    Frog Jump Space Optimized DP

--- code_studio_sol/Frog_Jump.cpp
+++ code_studio_sol/Frog_Jump.cpp
@@ -45,3 +45,20 @@ int frogJump(int n, vector<int> &heights)
     }
     return dp[n-1];
 }
+
+//M4 - Space Optimized Bottom Up DP
+#include<limits.h>
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    int prev = 0, prev2 = 0;
+    for(int i=1;i<n;i++){
+        int fs = prev+abs(heights[i]-heights[i-1]);
+        int ss = INT_MAX;
+        if(i>1) ss = prev2+abs(heights[i]-heights[i-2]);
+        int curr = min(fs, ss);
+        prev2 = prev;
+        prev = curr;
+    }
+    return prev;
+}
