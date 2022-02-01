d03aea2b8d27b7ca003dea66fa7f96e2bb717cfc
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-02-01 23:23:35 +0530

    Course Schedule II

---
+++ leetcode_sol/210-Course_Schedule_II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> ans;
+        vector<vector<int>> adj(numCourses);
+        vector<int> no_of_prereq(numCourses, 0);
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            no_of_prereq[prerequisites[i][0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<no_of_prereq.size();i++){
+            if(!no_of_prereq[i]) q.push(i);
+        }
+        int count_completed = 0;
+        while(!q.empty()){
+            int x = q.front();
+            ans.push_back(x);
+            q.pop();
+            count_completed++;
+            for(int i=0;i<adj[x].size();i++){
+                no_of_prereq[adj[x][i]]--;
+                if(!no_of_prereq[adj[x][i]]) q.push(adj[x][i]);
+            }
+        }
+        if(count_completed==numCourses) return ans;
+        return {};
+    }
+};
