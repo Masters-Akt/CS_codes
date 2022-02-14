4537aaab5c6eb4729a4fed6db577659ac8eaec1d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-02-14 10:35:28 +0530

    Count Operations To Obtain Zero

---
+++ leetcode_sol/2169-Count_Operations_To_Obtain_Zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int ans = 0;
+        while(num1!=0 && num2!=0){
+            ans++;
+            if(num1>=num2) num1-=num2;
+            else num2-=num1;
+        }
+        return ans;
+    }
+};
