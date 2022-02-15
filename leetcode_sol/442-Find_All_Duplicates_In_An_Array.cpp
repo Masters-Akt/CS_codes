3bc70a96b1108d634b808308cbf4f9dd8203c9a4
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-02-15 22:35:03 +0530

    Find All Duplicates In An Array

--- leetcode_sol/442-Find_All_Duplicates_In_An_Array.cpp
+++ leetcode_sol/442-Find_All_Duplicates_In_An_Array.cpp
@@ -1,11 +1,25 @@
+//TC = O(N) SC = O(N)
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        map<int, int> m;
+        unordered_map<int, int> m;
         vector<int> ans;
         for(int x: nums){
             if(m[x]==1) ans.push_back(x);
             m[x]++;
+        }
+        return ans;
+    }
+};
+
+//TC = O(N) SC = O(1)
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[abs(nums[i])-1]<0) ans.push_back(abs(nums[i]));
+            nums[abs(nums[i])-1]*=-1;
         }
         return ans;
     }
