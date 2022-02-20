8328fe55f4e31fda62bf911dd67c43ac17bd2c36
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-02-20 19:16:20 +0530

    Level Order Traversal

---
+++ gfg_sol/Level_Order_Traversal.cpp
@@ -0,0 +1,30 @@
+//Problem: https://practice.geeksforgeeks.org/problems/level-order-traversal/1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int solve(TreeNode* root, int v, int& ans){
+        if(!root) return 0;
+        int left = solve(root->left, root->val, ans);
+        int right = solve(root->right, root->val, ans);
+        ans = max(ans, left+right);
+        if(v==root->val) return 1+max(left, right);
+        return 0;
+    }
+public:
+    int longestUnivaluePath(TreeNode* root) {
+        if(!root) return 0;
+        int ans = 0;
+        solve(root, root->val, ans);
+        return ans;
+    }
+};
