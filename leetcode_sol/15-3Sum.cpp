9d122755158642066dab44a4436a397f95f109fd
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-07 01:49:56 +0530

    3 Sum

--- leetcode_sol/15-3Sum.cpp
+++ leetcode_sol/15-3Sum.cpp
@@ -21,3 +21,29 @@ public:
         return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
+
+//M2 - Accepted - Two Pointer
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.size()<3) return {};
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size()-2;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int high = nums.size()-1, low = i+1;
+                while(low<high){
+                    if(nums[low]+nums[high] == 0-nums[i]){
+                        ans.push_back({nums[i], nums[low], nums[high]});
+                        while(low<high && nums[low]==nums[low+1]) low++;
+                        while(low<high && nums[high]==nums[high-1]) high--;
+                        low++;
+                        high--;
+                    }else if(nums[low]+nums[high]<0-nums[i]) low++;
+                    else high--;
+                }
+            }
+        }
+        return ans;
+    }
+};
