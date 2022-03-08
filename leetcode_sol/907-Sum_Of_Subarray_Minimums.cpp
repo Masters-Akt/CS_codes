bed557311e1bf0ec0b921ebc13238726d80af1b0
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-08 18:19:43 +0530

    Sum Of Subarray Minimums

--- leetcode_sol/907-Sum_Of_Subarray_Minimums.cpp
+++ leetcode_sol/907-Sum_Of_Subarray_Minimums.cpp
@@ -14,3 +14,21 @@ public:
         return ans;
     }
 };
+
+//AC using Stack + DP
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        stack<int> st;
+        st.push(-1);
+        int mod = 1e9+7, ans = 0;
+        vector<int> dp(arr.size()+1, 0);
+        for(int i=0;i<arr.size();i++){
+            while(st.top()!=-1 && arr[i]<=arr[st.top()]) st.pop();
+            dp[i+1] = (dp[st.top()+1]+(i-st.top())*arr[i])%mod;
+            st.push(i);
+            ans=(ans+dp[i+1])%mod;
+        }
+        return ans;
+    }
+};
