6903d82c4c37e329b817583b7e89fe4620f2f72a
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-08 11:07:40 +0530

    Number of Islands

--- leetcode_sol/200-Number_Of_Islands.cpp
+++ leetcode_sol/200-Number_Of_Islands.cpp
@@ -25,3 +25,31 @@ public:
         return ans;
     }
 };
+
+//Same method but in a better way of writing
+class Solution {
+private:
+    void update(int i, int j, vector<vector<char>>& grid){
+        if(i>=grid.size() || i<0 || j>=grid[i].size() || j<0) return;
+        vector<int> dir = {0, 1, 0, -1, 0};
+        if(grid[i][j]=='1'){
+            grid[i][j] = '0';
+            for(int p=0;p<4;p++){
+                update(i+dir[p], j+dir[p+1], grid);
+            }
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+                if(grid[i][j]=='1'){
+                    ans++;
+                    update(i, j, grid);
+                }
+            }
+        }
+        return ans;
+    }
+};
