7175ccf1eb0b09378698f5b6e20d3ee7544e1bb9
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-08 19:49:59 +0530

    Number Of Matching Subsequences

--- leetcode_sol/792-Number_Of_Matching_Subsequences.cpp
+++ leetcode_sol/792-Number_Of_Matching_Subsequences.cpp
@@ -17,3 +17,24 @@ public:
         return ans;
     }
 };
+
+//store the indices of s and check for it - AC
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        vector<vector<int>> store(26);
+        for(int i=0;i<s.size();i++) store[s[i]-'a'].push_back(i);
+        int ans = 0;
+        for(string& word: words){
+            int pos = -1;
+            bool found = true;
+            for(char& c: word){
+                auto it = upper_bound(store[c-'a'].begin(), store[c-'a'].end(), pos);
+                if(it!=store[c-'a'].end()) pos = *it;
+                else found = false;
+            }
+            if(found) ans++;
+        }
+        return ans;
+    }
+};
