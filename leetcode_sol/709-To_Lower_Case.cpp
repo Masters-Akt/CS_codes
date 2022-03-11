57e7a245ee75aa9178463dbe5bea57c4ed679672
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-11 08:54:03 +0530

    To Lower Case

--- leetcode_sol/709-To_Lower_Case.cpp
+++ leetcode_sol/709-To_Lower_Case.cpp
@@ -13,3 +13,16 @@ public:
         return ans;
     }
 };
+
+//M2
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+            if(isupper(s[i])) ans+=tolower(s[i]);
+            else ans+=s[i];
+        }
+        return ans;
+    }
+};
