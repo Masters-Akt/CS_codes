6b72ee3bcd9ca8edd8fdee27a6bd86271f7d9cfe
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-16 11:26:32 +0530

    Plus_One.cpp

--- leetcode_sol/66-Plus_One.cpp
+++ leetcode_sol/66-Plus_One.cpp
@@ -1,27 +1,15 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int i=digits.length-1;
-        boolean done=false;
-        digits[i]++;
-        while(done==false&&i!=0){
-            if(digits[i]==digits[i]%10)
-                done=true;
-            else{
-                digits[i]=digits[i]%10;
-                digits[i-1]++;
-                i--;
-            }
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> res;
+        int carry = 1;
+        for(int i=digits.size()-1; i>=0; i--){
+            int sum = carry + digits[i];
+            carry = sum / 10;
+            res.push_back(sum % 10);
         }
-        if(digits[0]==digits[0]%10)
-        return digits;
-        else{
-            int res[]=new int[digits.length+1];
-            res[0]=digits[0]/10;
-            res[1]=digits[0]%10;
-            for(int j=1;j<digits.length;j++){
-                res[j+1]=digits[j];
-            }
-            return res;
-        }
+        if(carry) res.push_back(1);
+        reverse(res.begin(), res.end());
+        return res;
     }
-}
+};
