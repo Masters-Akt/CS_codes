550cfd84a143ccd4405d05a9a12847c2e580e2a0
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-21 23:34:04 +0530

    Intersection Of Two Arrays II

---
+++ leetcode_sol/350-Intersection_Of_Two_Arrays_II.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        nums1.erase(set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums1.begin()), nums1.end());
+        return nums1;
+    }
+};
