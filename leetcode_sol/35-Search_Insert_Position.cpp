4dd0b1b66e3058ee47e6b74c76d0c396eb15e98e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-21 19:27:10 +0530

    Search Insert Position

--- leetcode_sol/35-Search_Insert_Position.cpp
+++ leetcode_sol/35-Search_Insert_Position.cpp
@@ -14,3 +14,17 @@ public:
         return l;
     }
 };
+
+//Same
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while(low<=high){
+            int mid = (high+low)/2;
+            if(target>nums[mid]) low = mid+1;
+            else high = mid-1;
+        }
+        return low;
+    }
+};
