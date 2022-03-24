8c2f466a6de0e340a3066a7bcb8427ea39727fe3
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-03-24 13:50:02 +0530

    Sort Colors

--- leetcode_sol/75-Sort_Colors.cpp
+++ leetcode_sol/75-Sort_Colors.cpp
@@ -26,3 +26,15 @@ public:
         }
     }
 };
+//OR
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red = 0, white = 0, blue = nums.size()-1;
+        while(white<=blue){
+            if(nums[white]==0) swap(nums[red++], nums[white++]);
+            else if(nums[white]==1) white++;
+            else swap(nums[white], nums[blue--]);
+        }
+    }
+};
