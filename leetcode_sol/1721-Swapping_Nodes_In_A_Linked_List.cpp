6f162d1b16ab0188349cda04e119eae97d189117
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-04-04 08:58:26 +0530

    Swapping Nodes In A Linked List

--- leetcode_sol/1721-Swapping_Nodes_In_A_Linked_List.cpp
+++ leetcode_sol/1721-Swapping_Nodes_In_A_Linked_List.cpp
@@ -1,16 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 //Method 1
-/*
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
@@ -38,36 +26,20 @@ public:
         return head;
     }
 };
-*/
 
 //Method 2 using two pointers
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* first;
         ListNode* fast = head;
         ListNode* slow = head;
-        for(int i=1;i<k;i++){
-            fast = fast->next;
-        }
-        first = fast;
+        for(int i=1;i<k;i++) fast = fast->next;
+        ListNode* first = fast;
         while(fast->next!=NULL){
             fast = fast->next;
             slow = slow->next;
         }
-        int temp = first->val;
-        first->val = slow->val;
-        slow->val = temp;
+        swap(first->val, slow->val);
         return head;
     }
 };
