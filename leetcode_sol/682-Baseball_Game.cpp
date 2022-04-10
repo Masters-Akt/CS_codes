8ef2bd2eac9a9a6720fdf9d19930b0382dba619e
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-04-10 23:13:55 +0530

    Baseball Game

--- leetcode_sol/682-Baseball_Game.cpp
+++ leetcode_sol/682-Baseball_Game.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
@@ -23,5 +24,20 @@ public:
             s.pop();
         }
         return sum;
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        vector<int> record;
+        for(string ch: ops){
+            if (ch == "C") record.pop_back();
+            else if (ch == "D") record.push_back(record.back() * 2);
+            else if (ch == "+") record.push_back(record.back() + record[record.size() - 2]);
+            else record.push_back(stoi(ch));
+        }
+        return accumulate(record.begin(), record.end(), 0);
     }
 };
