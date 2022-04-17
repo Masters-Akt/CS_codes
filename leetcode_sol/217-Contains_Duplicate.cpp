1ad59e00716bfab7f37853e13540375e4b773e0d
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-04-17 16:10:53 +0530

    Contains Duplicate

--- leetcode_sol/217-Contains_Duplicate.cpp
+++ leetcode_sol/217-Contains_Duplicate.cpp
@@ -22,3 +22,16 @@ public:
         return false;
     }
 };
+
+//M3
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, bool> m;
+        for(int i=0;i<nums.size();i++){
+            if(m[nums[i]]) return true;
+            m[nums[i]] = true;
+        }
+        return false;
+    }
+};
