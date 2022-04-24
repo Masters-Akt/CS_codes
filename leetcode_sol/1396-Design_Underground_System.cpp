aea9e1f01fa3a0add567a7aa59daa06432f5093b
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-04-25 00:37:17 +0530

    Design Underground System

---
+++ leetcode_sol/1396-Design_Underground_System.cpp
@@ -0,0 +1,35 @@
+class UndergroundSystem {
+private:
+    unordered_map<int, pair<string, int>> checkInMap;
+    unordered_map<string, pair<double, int>> routeMap;
+public:
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        checkInMap[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        auto checkIn = checkInMap[id];
+        checkInMap.erase(id);
+        string routeName = checkIn.first + "_" + stationName;
+        routeMap[routeName].first += t - checkIn.second;
+        routeMap[routeName].second += 1;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        string routeName = startStation + "_" + endStation;
+        auto& route = routeMap[routeName];
+        return route.first / route.second;
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
