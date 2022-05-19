6ec8e230ed0074b6cfde7852d71f2e544c98a82f
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-05-19 23:12:52 +0530

    Longest Increasing Path In A Matrix

---
+++ leetcode_sol/329-Longest_Increasing_Path_In_A_Matrix.cpp
@@ -0,0 +1,25 @@
+//M1 - TLE
+class Solution {
+private:
+    int solve(vector<vector<int>>& matrix, int i, int j){
+        int ans = 1;
+        vector<int> dir = {0, 1, 0, -1, 0};
+        for(int d=0;d<4;d++){
+            int xi = i+dir[d], yi = j+dir[d+1];
+            if(xi>=0 && yi>=0 && xi<matrix.size() && yi<matrix[0].size() && matrix[xi][yi]>matrix[i][j]){
+                ans = max(ans, solve(matrix, xi, yi)+1);
+            }
+        }
+        return ans;
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int ans = 1;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                ans = max(ans, solve(matrix, i, j));
+            }
+        }
+        return ans;
+    }
+};
