666e569a26cddd1f98e9c729190f48959be82b91
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-05-29 11:10:51 +0530

    Steps To Make Array Non-Dereasing

--- leetcode_sol/2289-Steps_To_Make_Array_Non-Decreasing.cpp
+++ leetcode_sol/2289-Steps_To_Make_Array_Non-Decreasing.cpp
@@ -1,22 +1,18 @@
-//M1 - Brute Force - TLE
 class Solution {
 public:
     int totalSteps(vector<int>& nums) {
         int total_steps = 0;
-        while(1){
-            vector<int> index_to_remove;
-            for(int i=1;i<nums.size();i++){
-                if(nums[i]<nums[i-1]){
-                    index_to_remove.push_back(i);
-                }
+        stack<pair<int, int>> st;
+        int n = nums.size();
+        st.push({nums[n-1], 0});
+        for(int i=n-2;i>=0;i--){
+            int max_steps_till_now = 0;
+            while(!st.empty() && nums[i]>st.top().first){
+                max_steps_till_now = max(max_steps_till_now+1, st.top().second);
+                st.pop();
             }
-            if(index_to_remove.size()==0) break;
-            else{
-                total_steps++;
-                for(int i=index_to_remove.size()-1;i>=0;i--){
-                    nums.erase(nums.begin()+index_to_remove[i]);
-                }
-            }
+            total_steps = max(total_steps, max_steps_till_now);
+            st.push({nums[i], max_steps_till_now});
         }
         return total_steps;
     }
