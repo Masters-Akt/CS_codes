6ff132d6a4613df00d09abab6c1c61a9240357c7
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-06-01 07:11:13 +0530

    Running Sum Of 1D Array

--- leetcode_sol/1480-Running_Sum_Of_1D_Array.cpp
+++ leetcode_sol/1480-Running_Sum_Of_1D_Array.cpp
@@ -1,3 +1,4 @@
+//O(N)
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
@@ -6,6 +7,19 @@ public:
         for(int i=0;i<nums.size();i++){
             s=s+nums[i];
             sum.push_back(s);
+        }
+        return sum;
+    }
+};
+
+//O(N) more short
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> sum(nums.size());
+        sum[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum[i] = nums[i]+sum[i-1];
         }
         return sum;
     }
