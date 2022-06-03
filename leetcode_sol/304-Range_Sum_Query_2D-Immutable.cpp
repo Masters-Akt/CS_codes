7a453628b90787be85c5d9323cc0868cb9db9ef5
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-06-03 23:39:01 +0530

    Range Sum Query 2D Immutable

--- leetcode_sol/304-Range_Sum_Query_2D-Immutable.cpp
+++ leetcode_sol/304-Range_Sum_Query_2D-Immutable.cpp
@@ -17,3 +17,36 @@ public:
         return sum;
     }
 };
+
+//M2 - Using Row as Prefix sum - TC=O(m)
+class NumMatrix {
+private:
+    vector<vector<int>> mat;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+       int m = matrix.size(), n = matrix[0].size();
+        for(int i=0;i<m;i++){
+            vector<int> row(n);
+            row[0] = matrix[i][0];
+            for(int j=1;j<n;j++){
+                row[j] = row[j-1] + matrix[i][j];
+            }
+            mat.push_back(row);
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0, minus = 0;
+        for(int i=row1;i<=row2;i++){
+            sum += mat[i][col2];
+            if(col1>0) minus += mat[i][col1-1];
+        }
+        return sum - minus;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
