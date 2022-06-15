c6ce8d53464b45a8c5e0ea54e4b12549be6bf0a8
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-06-15 14:06:40 +0530

    Longest String Chain

--- leetcode_sol/1048-Longest_String_Chain.cpp
+++ leetcode_sol/1048-Longest_String_Chain.cpp
@@ -11,7 +11,7 @@ public:
         for(string x: words){
             for(int i=0;i<x.size();i++){
                 string prev = x.substr(0, i)+x.substr(i+1);
-                dp[x] = max(dp[x], dp[prev]==0 ? 1 : dp[prev]+1);
+                dp[x] = max(dp[x], dp[prev]+1);
             }
             ans = max(ans, dp[x]);
         }
