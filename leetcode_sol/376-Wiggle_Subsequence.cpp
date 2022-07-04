ccf16b6e0ac57231489555dfd1df63e922753b7c
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-07-04 16:32:46 +0530

    Wiggle Subsequence

---
+++ leetcode_sol/376-Wiggle_Subsequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) down = up + 1;
+            else if (nums[i] > nums[i - 1]) up = down + 1;
+        }
+        return max(up, down);
+    }
+};
