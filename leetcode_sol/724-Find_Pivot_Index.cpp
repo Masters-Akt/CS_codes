b0ae313ee70bfcbf0b31390a2178254bd3b72427
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-07-19 12:51:47 +0530

    Find Pivot Index

--- leetcode_sol/724-Find_Pivot_Index.cpp
+++ leetcode_sol/724-Find_Pivot_Index.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int f[nums.size()], b[nums.size()];
+        vector<int> prefixSum(nums.size()), suffixSum(nums.size());
         int sum = 0;
         for(int i=0;i<nums.size();i++){
-            f[i] = sum;
+            prefixSum[i] = sum;
             sum+=nums[i];
         }
         sum = 0;
         for(int i=nums.size()-1;i>=0;i--){
-            b[i] = sum;
+            suffixSum[i] = sum;
             sum+=nums[i];
         }
         for(int i=0;i<nums.size();i++){
-            if(f[i]==b[i]) return i;
+            if(prefixSum[i]==suffixSum[i]) return i;
         }
         return -1;
     }
