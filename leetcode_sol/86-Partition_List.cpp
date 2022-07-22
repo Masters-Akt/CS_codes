5d0db98d6b1a7e4ed3ccdaeea3db48e2c96fec82
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-07-22 23:42:40 +0530

    Partition List

---
+++ leetcode_sol/86-Partition_List.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *left = new ListNode(0);
+        ListNode *right = new ListNode(0);
+        
+        ListNode *leftTail = left;
+        ListNode *rightTail = right;
+        
+        while(head != NULL){
+            if(head->val < x){
+                leftTail->next = head;
+                leftTail = leftTail->next;
+            }
+            else{
+                rightTail->next = head;
+                rightTail = rightTail->next;
+            }
+            head = head->next;
+        }
+        
+        leftTail->next = right->next;
+        rightTail->next = NULL;
+        
+        return left->next;
+    }
+};
