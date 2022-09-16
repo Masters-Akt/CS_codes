ce8a11fb0ddf465c6e13d947a283d00b243d1505
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-09-17 00:23:29 +0530

    Maximum Score from Performing Multiplication Operations

---
+++ leetcode_sol/1770-Maximum_Score_From_Performing_Multiplication_Operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<vector<int>> dp;
+    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
+        if (j == M.size()) return 0;
+        if (dp[i][j] != INT_MIN) return dp[i][j];
+        // Left Side
+        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
+        // Right Side
+        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
+        return dp[i][j] = max(left, right);
+    }
+public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = nums.size(), m = multipliers.size();
+        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
+        return solve(0, n, 0, nums, multipliers);
+    }
+};
