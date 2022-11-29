beeeb8a5e0e1c6384e7949f4615e37409aeb1c80
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-11-29 23:14:15 +0530

    Insert Delete GetRandom O(1)

---
+++ leetcode_sol/380-Insert_Delete_GetRandom_O(1).cpp
@@ -0,0 +1,38 @@
+class RandomizedSet {
+private:
+    vector<int> nums;
+    unordered_map<int, int> m;
+public:
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if (m.find(val) != m.end()) return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (m.find(val) == m.end()) return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
+        m.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
