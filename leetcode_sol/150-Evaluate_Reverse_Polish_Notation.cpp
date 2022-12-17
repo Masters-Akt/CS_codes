a43ba746fa9e00752d3a05f8e2fc6c09e4987c01
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2022-12-17 23:00:54 +0530

    Evaluate Reverse Polish Notation

--- leetcode_sol/150-Evaluate_Reverse_Polish_Notation.cpp
+++ leetcode_sol/150-Evaluate_Reverse_Polish_Notation.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<string> st;
-        for(int i=0;i<tokens.size();i++){
-            if(st.empty()) st.push(tokens[i]);
-            else if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
-                string a = st.top();
-                st.pop();
-                string b = st.top();
-                st.pop();
-                int ans = 0;
-                if(tokens[i]=="+") ans = stoi(b)+stoi(a);
-                else if(tokens[i]=="-") ans = stoi(b)-stoi(a);
-                else if(tokens[i]=="*") ans = stoi(b)*stoi(a);
-                else ans = stoi(b)/stoi(a);
-                st.push(to_string(ans));
-            }else st.push(tokens[i]);
-        }
-        return stoi(st.top());
+        string s = tokens.back(); tokens.pop_back();
+        if(s != "+" && s != "-" && s != "*" && s != "/") return stoi(s);
+        
+        long r2 = evalRPN(tokens), r1 = evalRPN(tokens);
+        
+        if(s == "+") return r1 + r2;
+        if(s == "-") return r1 - r2;
+        if(s == "*") return r1 * r2;
+        return r1 / r2;
     }
 };
