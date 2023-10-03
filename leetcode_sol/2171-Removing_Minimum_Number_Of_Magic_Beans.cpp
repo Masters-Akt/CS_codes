db564951713e7ed108d72d28f78e1f29bce48a7f
Kumar Ankit <64123046+Masters-Akt@users.noreply.github.com>
2023-10-04 02:29:05 +0530

    2171-Removing_Minimum_Number_Of_Magic_Beans.cpp
    2171 updated

--- leetcode_sol/2171-Removing_Minimum_Number_Of_Magic_Beans.cpp
+++ leetcode_sol/2171-Removing_Minimum_Number_Of_Magic_Beans.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     long long minimumRemoval(vector<int>& beans) {
@@ -13,6 +14,22 @@ public:
         for(int i=0;i<beans.size();i++){
             long long temp = suffix[i]+sum-suffix[i+1]-(beans.size()-i-1)*beans[i];
             ans = min(ans, temp);
+        }
+        return ans;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    //Expl: After arrangement, make each one a candidate and find the min value for: at each place I need to drop all the less values ones and subtract the current from more value ones 
+    long long minimumRemoval(vector<int>& beans) {
+        sort(beans.begin(), beans.end(), greater<int>());
+        long long ans = LONG_LONG_MAX;
+        long long total = 0;
+        for(int x: beans) total+=x;
+        for(int i=0;i<beans.size();i++){
+            ans = min(ans, total - (long long)beans[i]*(i+1));
         }
         return ans;
     }
